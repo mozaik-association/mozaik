6d65d7a560badca7aea15cd80a4bd4e4f148c81c
Olivier Laurent <olivier.laurent@acsone.eu>
2014-03-13 02:31:55 +0100

    [CHG] prevent coordinate category for deletion

--- ficep_coordinate/abstract_coordinate.py
+++ ficep_coordinate/abstract_coordinate.py
@@ -57,7 +57,7 @@ class abstract_coordinate(orm.AbstractModel):
         'id': fields.integer('ID', readonly=True),
 
         'partner_id': fields.many2one('res.partner', 'Contact', readonly=True, required=True, select=True),
-        'coordinate_category_id': fields.many2one('coordinate.category', 'Coordinate Category', select=True, track_visibility='onchange'),
+        'coordinate_category_id': fields.many2one('coordinate.category', 'Coordinate Category', select=True, ondelete='restrict', track_visibility='onchange'),
         'coordinate_type': fields.selection(COORDINATE_AVAILABLE_TYPES, 'Coordinate Type'),
 
         'is_main': fields.boolean('Is Main', readonly=True, select=True),
