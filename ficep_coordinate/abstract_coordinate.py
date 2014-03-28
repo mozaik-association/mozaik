7aa1ff30804cb4e652d369c0f9f0c6109ae84c37
Jonathan Nemry <jonathan.nemry@acsone.eu>
2014-03-28 16:29:53 +0100

    [CHG] delete unused

--- ficep_coordinate/abstract_coordinate.py
+++ ficep_coordinate/abstract_coordinate.py
@@ -25,8 +25,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-from openerp.tools import SUPERUSER_ID
 from openerp.osv import orm, fields
 from openerp.tools.translate import _
 
