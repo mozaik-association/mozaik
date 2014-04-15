8dbf47dc658f341871878baf7605ad41fee8f405
Cédric Pigeon <cedric.pigeon@acsone.eu>
2014-04-15 21:54:51 +0200

    [ADD] add wizard to renew or create a derived mandate (only for state mandate for the moment)

--- ficep_mandate/wizard/__init__.py
+++ ficep_mandate/wizard/__init__.py
@@ -25,42 +25,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{
-    'name': 'FICEP: Mandate',
-    'version': '1.0',
-    "author": "ACSONE SA/NV",
-    "maintainer": "ACSONE SA/NV",
-    "website": "http://www.acsone.eu",
-    'category': 'Political Association',
-    'depends': [
-        'ficep_structure',
-    ],
-    'description': """
-FICEP Mandate
-=============
-    """,
-    'images': [
-    ],
-    'data': [
-             'abstract_mandate_view.xml',
-             'mandate_view.xml',
-             'sta_mandate_workflow.xml',
-             'wizard/renew_and_derived_mandate_wizard.xml',
-    ],
-    'js': [
-    ],
-    'qweb': [
-    ],
-    'css': [
-    ],
-    'demo': [
-    ],
-    'test': [
-    ],
-    'sequence': 150,
-    'active': False,
-    'auto_install': False,
-    'installable': True,
-}
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+from . import renew_and_derived_mandate_wizard
