1caee6b5c84d61d246fd478443ec48ad25601879
Cédric Pigeon <cedric.pigeon@acsone.eu>
2014-06-20 16:33:35 +0200

    [FIX] change store value on related fields + change workflow + designation_in_assembly_id not readonly on committee and mandate

--- ficep_mandate/ext_mandate.py
+++ ficep_mandate/ext_mandate.py
@@ -150,16 +150,49 @@ class ext_candidature(orm.Model):
     _allowed_inactive_link_models = [_selection_committee_model]
     _mandate_form_view = 'ext_mandate_form_view'
 
+    _mandate_category_store_trigger = {
+        'ext.candidature': (lambda self, cr, uid, ids, context=None: ids, ['selection_committee_id'], 20),
+        _selection_committee_model: (lambda self, cr, uid, ids, context=None:
+                                    self.pool.get('ext.candidature').search(cr, uid, [('selection_committee_id', 'in', ids)], context=context),
+                                    ['mandate_category_id'], 20),
+    }
+
+    _ext_assembly_store_trigger = {
+        'ext.candidature': (lambda self, cr, uid, ids, context=None: ids, ['selection_committee_id'], 20),
+        _selection_committee_model: (lambda self, cr, uid, ids, context=None:
+                                    self.pool.get('ext.candidature').search(cr, uid, [('selection_committee_id', 'in', ids)], context=context),
+                                    ['ext_assembly_id'], 20),
+    }
+
+    _designation_assembly_store_trigger = {
+        'ext.candidature': (lambda self, cr, uid, ids, context=None: ids, ['selection_committee_id'], 20),
+        _selection_committee_model: (lambda self, cr, uid, ids, context=None:
+                                    self.pool.get('ext.candidature').search(cr, uid, [('selection_committee_id', 'in', ids)], context=context),
+                                    ['designation_int_assembly_id'], 20),
+    }
+
+    _mandate_start_date_store_trigger = {
+        'ext.candidature': (lambda self, cr, uid, ids, context=None: ids, ['selection_committee_id'], 20),
+        _selection_committee_model: (lambda self, cr, uid, ids, context=None:
+                                    self.pool.get('ext.candidature').search(cr, uid, [('selection_committee_id', 'in', ids)], context=context),
+                                    ['mandate_start_date'], 20),
+    }
+
     _columns = {
         'state': fields.selection(CANDIDATURE_AVAILABLE_STATES, 'Status', readonly=True, track_visibility='onchange',),
         'selection_committee_id': fields.many2one(_selection_committee_model, string='Selection Committee',
                                                  required=True, select=True, track_visibility='onchange'),
         'mandate_category_id': fields.related('selection_committee_id', 'mandate_category_id', string='Mandate Category',
                                           type='many2one', relation="mandate.category",
-                                          store=True, domain=[('type', '=', 'ext')]),
+                                          store=_mandate_category_store_trigger, domain=[('type', '=', 'ext')]),
+        'mandate_start_date': fields.related('selection_committee_id', 'mandate_start_date', string='Mandate Start Date',
+                                          type='date', store=_mandate_start_date_store_trigger),
         'ext_assembly_id': fields.related('selection_committee_id', 'assembly_id', string='External Assembly',
                                           type='many2one', relation="ext.assembly",
-                                          store=True),
+                                          store=_ext_assembly_store_trigger),
+        'designation_int_assembly_id': fields.related('selection_committee_id', 'designation_int_assembly_id', string='Designation Assembly',
+                                          type='many2one', relation="int.assembly",
+                                          store=_designation_assembly_store_trigger),
         'months_before_end_of_mandate': fields.related('ext_assembly_id', 'months_before_end_of_mandate', string='Months before end of Mandate',
                                           type='integer', relation="ext.assembly",
                                           store=False),
