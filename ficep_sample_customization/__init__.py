f97acb7ec847612ea64e6df7729ab5f3a1527705
Olivier LAURENT <olivier.laurent@acsone.eu>
2014-07-10 21:39:42 +0200

    [FIX] missing referenced .py file

--- ficep_sample_customization/__init__.py
+++ ficep_sample_customization/__init__.py
@@ -25,6 +25,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import wizard_multi_charts_accounts
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
