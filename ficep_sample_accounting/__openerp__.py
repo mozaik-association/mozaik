36b3a84163430427830f2001b16abde3e8488c4f
Cédric Pigeon <cedric.pigeon@acsone.eu>
2014-08-06 15:10:47 +0200

    [CHG] split sample customization and accounting sample

--- ficep_sample_accounting/__openerp__.py
+++ ficep_sample_accounting/__openerp__.py
@@ -26,44 +26,24 @@
 #
 ##############################################################################
 {
-    'name': 'FICEP: Sample Customization',
+    'name': 'FICEP: Sample Accounting',
     'version': '1.0',
     "author": "ACSONE SA/NV",
     "maintainer": "ACSONE SA/NV",
     "website": "http://www.acsone.eu",
     'category': 'Political Association',
     'depends': [
-        'ficep',
+        'ficep_sample_customization',
+        'l10n_ficep',
     ],
     'description': """
-FICEP Sample Customization
+FICEP Sample Accounting
 ==========================
     """,
     'images': [
     ],
     'data': [
-         'demo/company_demo.xml',
-         'demo/users_demo.xml',
-         '../ficep_base/tests/data/res_partner_data.xml',
-         '../ficep_base/tests/data/res_users_data.xml',
-         '../ficep_structure/tests/data/structure_data.xml',
-         '../ficep_person/tests/data/res_partner_data.xml',
-         '../ficep_person/tests/data/partner_involvment_data.xml',
-         '../ficep_coordinate/tests/data/coordinate_category_data.xml',
-         '../ficep_email/tests/data/email_data.xml',
-         '../ficep_address/tests/data/reference_data.xml',
-         '../ficep_address/tests/data/address_data.xml',
-         '../ficep_phone/tests/data/phone_data.xml',
-         '../ficep_person_coordinate/tests/data/relation_data.xml',
-         '../ficep_thesaurus/tests/data/thesaurus_data.xml',
-         '../ficep_membership/tests/data/structure_address_data.xml',
-         '../ficep_mandate/tests/data/mandate_data.xml',
-         '../ficep_retrocession/tests/data/retrocession_data.xml',
-         'demo/membership_request_demo.xml',
-         '../ficep_membership/tests/data/membership_request_data.xml',
-         '../ficep_communication/tests/data/communication_data.xml',
-         '../ficep_communication/tests/data/postal_mail_data.xml',
-         'demo/sample_customization_demo.xml',  # must be the last
+         '../ficep_account/tests/data/account_installer.xml',
     ],
     'js': [
     ],
