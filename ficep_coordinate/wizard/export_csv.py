7a68bb852aa790d060dcaa27369f2d7899d4890f
Olivier LAURENT <olivier.laurent@acsone.eu>
2014-08-19 20:06:01 +0200

    [PEP8]

--- ficep_coordinate/wizard/export_csv.py
+++ ficep_coordinate/wizard/export_csv.py
@@ -28,11 +28,10 @@
 
 import tempfile
 import csv
-import tempfile
 from collections import OrderedDict
 
 from openerp.tools.translate import _
-from openerp.osv import orm, fields
+from openerp.osv import orm
 
 from openerp.addons.ficep_person.res_partner import available_genders, available_tongues
 
@@ -95,11 +94,11 @@ class export_csv(orm.TransientModel):
 
     def get_csv_values(self, cr, uid, model, obj, context=None):
         """
-        ===============
+        ==============
         get_csv_values
-        ===============
-        Get the values of the specified obj, which should be an instance of the specified model.
-        The model should be either an email or postal coordinate.
+        ==============
+        Get the values of the specified obj, which should be an instance
+        of the specified model, either an email or a postal coordinate.
         """
 
         def safe_get(o, attr, default=None):
@@ -119,11 +118,12 @@ class export_csv(orm.TransientModel):
         if not partner:
             return False
 
-        # If we have a postal coordinate, we get the mail coordinates from the partner, and vice versa.
         if model == 'postal.coordinate':
+            # for postal coordinates, get email coordinate from the partner
             pc = obj
             ec = partner.email_coordinate_id or None
         elif model == 'email.coordinate':
+            # for email coordinates, get postal coordinate from the partner
             ec = obj
             pc = partner.postal_coordinate_id or None
 
@@ -134,44 +134,46 @@ class export_csv(orm.TransientModel):
         cc = pc and pc.co_residency_id
         ic = partner.int_instance_id
 
-        export_values = OrderedDict([('identifier', partner.identifier or None),
-                                     ('name', _get_utf8(partner.name)),
-                                     ('lastname', _get_utf8(partner.lastname)),
-                                     ('firstname', _get_utf8(partner.firstname)),
-                                     ('usual_lastname', _get_utf8(partner.usual_lastname)),
-                                     ('usual_firstname', _get_utf8(partner.usual_firstname)),
-                                     ('printable_name', cc and _get_utf8(cc.line) or _get_utf8(partner.printable_name)),
-                                     ('co_residency', cc and _get_utf8(cc.line2)),
-                                     ('adr_main', pc and pc.is_main or False),
-                                     ('adr_unauthorized', pc and pc.unauthorized or False),
-                                     ('adr_vip', pc and pc.vip or False),
-                                     ('country_code', pc and pc.address_id.country_code),
-                                     ('country_name', pc and _get_utf8(pc.address_id.country_id.name)),
-                                     ('zip', pc and pc.address_id.zip),
-                                     ('street', pc and _get_utf8(pc.address_id.street)),
-                                     ('street2', pc and _get_utf8(pc.address_id.street2)),
-                                     ('city', pc and _get_utf8(pc.address_id.city)),
-                                     ('instance', ic and _get_utf8(ic.name)),
-                                     ('birth_date', partner.birth_date or None),
-                                     ('gender', available_genders.get(partner.gender, None)),
-                                     ('tongue', available_tongues.get(partner.tongue, None)),
-                                     ('fix_main', xc and xc.is_main or False),
-                                     ('fix_unauthorized', xc and xc.unauthorized or False),
-                                     ('fix_vip', xc and xc.vip or False),
-                                     ('fix', xc and _get_utf8(xc.phone_id.name)),
-                                     ('mobile_main', mc and mc.is_main or False),
-                                     ('mobile_unauthorized', mc and mc.unauthorized or False),
-                                     ('mobile_vip', mc and mc.vip or False),
-                                     ('mobile', mc and _get_utf8(mc.phone_id.name)),
-                                     ('fax_main', fc and fc.is_main or False),
-                                     ('fax_unauthorized', fc and fc.unauthorized or False),
-                                     ('fax_vip', fc and fc.vip or False),
-                                     ('fax', fc and _get_utf8(fc.phone_id.name)),
-                                     ('email_main', ec and ec.is_main or False),
-                                     ('email_unauthorized', ec and ec.unauthorized or False),
-                                     ('email_vip', ec and ec.vip or False),
-                                     ('email', ec and _get_utf8(ec.email)),
-                                     ])
+        export_values = OrderedDict([
+            ('identifier', partner.identifier or None),
+             ('name', _get_utf8(partner.name)),
+             ('lastname', _get_utf8(partner.lastname)),
+             ('firstname', _get_utf8(partner.firstname)),
+             ('usual_lastname', _get_utf8(partner.usual_lastname)),
+             ('usual_firstname', _get_utf8(partner.usual_firstname)),
+             ('printable_name', cc and _get_utf8(cc.line) or
+                                _get_utf8(partner.printable_name)),
+             ('co_residency', cc and _get_utf8(cc.line2)),
+             ('adr_main', pc and pc.is_main or False),
+             ('adr_unauthorized', pc and pc.unauthorized or False),
+             ('adr_vip', pc and pc.vip or False),
+             ('country_code', pc and pc.address_id.country_code),
+             ('country_name', pc and _get_utf8(pc.address_id.country_id.name)),
+             ('zip', pc and pc.address_id.zip),
+             ('street', pc and _get_utf8(pc.address_id.street)),
+             ('street2', pc and _get_utf8(pc.address_id.street2)),
+             ('city', pc and _get_utf8(pc.address_id.city)),
+             ('instance', ic and _get_utf8(ic.name)),
+             ('birth_date', partner.birth_date or None),
+             ('gender', available_genders.get(partner.gender, None)),
+             ('tongue', available_tongues.get(partner.tongue, None)),
+             ('fix_main', xc and xc.is_main or False),
+             ('fix_unauthorized', xc and xc.unauthorized or False),
+             ('fix_vip', xc and xc.vip or False),
+             ('fix', xc and _get_utf8(xc.phone_id.name)),
+             ('mobile_main', mc and mc.is_main or False),
+             ('mobile_unauthorized', mc and mc.unauthorized or False),
+             ('mobile_vip', mc and mc.vip or False),
+             ('mobile', mc and _get_utf8(mc.phone_id.name)),
+             ('fax_main', fc and fc.is_main or False),
+             ('fax_unauthorized', fc and fc.unauthorized or False),
+             ('fax_vip', fc and fc.vip or False),
+             ('fax', fc and _get_utf8(fc.phone_id.name)),
+             ('email_main', ec and ec.is_main or False),
+             ('email_unauthorized', ec and ec.unauthorized or False),
+             ('email_vip', ec and ec.vip or False),
+             ('email', ec and _get_utf8(ec.email)),
+        ])
         return export_values
 
     def get_csv(self, cr, uid, model, model_ids, group_by=False, context=None):
@@ -186,15 +188,18 @@ class export_csv(orm.TransientModel):
             return
 
         objects = self.pool[model].browse(cr, uid, model_ids, context=context)
-        tmp = tempfile.NamedTemporaryFile(prefix='Extract', suffix=".csv", delete=False)
+        tmp = tempfile.NamedTemporaryFile(prefix='Extract', suffix=".csv",
+                                          delete=False)
         f = open(tmp.name, "r+")
         writer = csv.writer(f)
         writer.writerow(self.get_csv_rows(cr, uid, model, context=context))
         co_residencies = []
         for obj in objects:
             if model == 'postal.coordinate':
-                #case group by is ask and where a co_residency is present and already write then don't write a new row
-                if group_by and obj.co_residency_id and obj.co_residency_id.id in co_residencies:
+                # when grouping by co_residency, output only one row
+                # by co_residency
+                if group_by and obj.co_residency_id and \
+                   obj.co_residency_id.id in co_residencies:
                     continue
                 co_residencies.append(obj.co_residency_id.id)
 
@@ -216,10 +221,10 @@ class export_csv(orm.TransientModel):
 
         # Send to current user
         attachment = [(_('Extract.csv'), '%s' % csv_content)]
-        partner_ids = self.pool['res.partner'].search(cr, uid, [('user_ids', '=', uid)], context=context)
+        partner_ids = self.pool['res.partner'].search(
+            cr, uid, [('user_ids', '=', uid)], context=context)
         if partner_ids:
-            self.pool['mail.thread'].message_post(cr, uid, False, attachments=attachment, context=context,
-                                                  partner_ids=partner_ids, subject=_('Export CSV'))
+            self.pool['mail.thread'].message_post(cr, uid,
+                False, attachments=attachment, context=context,
+                partner_ids=partner_ids, subject=_('Export CSV'))
         return True
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
