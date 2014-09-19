75a4291fa5fca5e3085e3a15b9569b075819b845
Olivier LAURENT <olivier.laurent@acsone.eu>
2014-09-19 19:13:47 +0200

    [MOV] involment demo data to sample custom.

--- ficep_sample_customization/__openerp__.py
+++ ficep_sample_customization/__openerp__.py
@@ -47,7 +47,6 @@ FICEP Sample Customization
         '../ficep_base/tests/data/res_users_data.xml',
         '../ficep_structure/tests/data/structure_data.xml',
         '../ficep_person/tests/data/res_partner_data.xml',
-        '../ficep_person/tests/data/partner_involvment_data.xml',
         '../ficep_coordinate/tests/data/coordinate_category_data.xml',
         '../ficep_email/tests/data/email_data.xml',
         '../ficep_address/tests/data/reference_data.xml',
