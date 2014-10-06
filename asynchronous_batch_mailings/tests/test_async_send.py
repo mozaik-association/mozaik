bdc144094fda6116bf164711834be43cefe914c0
Jonathan Nemry <jonathan.nemry@acsone.eu>
2014-10-06 11:21:26 +0200

    [acsone-addons][asynchronous_batch_mailings][NEW] This Module allows to send email by an asynchronous way. It also provides a way to split huge mailing. [acsone-addons][CHG] add dependency on connector in .travis.yml

---
+++ asynchronous_batch_mailings/tests/test_async_send.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Authors: Nemry Jonathan
+#    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
+#    All Rights Reserved
+#
+#    WARNING: This program as such is intended to be used by professional
+#    programmers who take the whole responsibility of assessing all potential
+#    consequences resulting from its eventual inadequacies and bugs.
+#    End users who are looking for a ready-to-use solution with commercial
+#    guarantees and support are strongly advised to contact a Free Software
+#    Service Company.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+
+from openerp.tests import common
+from openerp.addons.connector.session import ConnectorSession
+from openerp.addons.asynchronous_batch_mailings.wizard import \
+    mail_compose_message as fc
+
+_logger = logging.getLogger(__name__)
+
+DB = common.DB
+ADMIN_USER_ID = common.ADMIN_USER_ID
+
+
+class test_async_mail_send(common.TransactionCase):
+
+    def setUp(self):
+        super(test_async_mail_send, self).setUp()
+
+        self.registry('ir.model').clear_caches()
+        self.registry('ir.model.data').clear_caches()
+
+    def test_async_send_mail(self):
+        """
+        This test will check that the method create a right mail.mail
+        from the passing values of the wizard.
+        """
+        cr, uid = self.cr, self.uid
+        context = {
+            'lang': 'en_US',
+            'tz': False,
+            'uid': 1,
+            'show_reload': True,
+            'default_composition_mode': 'mass_mail',
+            'default_model': 'res.partner'
+        }
+        ir_att_obj = self.registry['ir.attachment']
+        vals = {
+            'datas': 'bWlncmF0aW9uIHRlc3Q=',
+            'datas_fname': 'RedHat_spec.doc',
+            'name': 'RedHat_spec.doc',
+        }
+        attachment_id = ir_att_obj.create(cr, uid, vals, context=context)
+        vals = {
+            'name': 'Mitch',
+            'email': 'mitch@mi.tch',
+        }
+        partner_id = self.registry['res.partner'].create(
+            cr, uid, vals, context=context)
+
+        mail_vals = {
+            'composition_mode': 'mass_mail',
+            'body': '<p>sample body</p>',
+            'partner_ids': [partner_id],
+            'use_active_domain': False,
+            'attachment_ids': [[6, False, [attachment_id]]],
+            'template_id': 1,
+            'parent_id': False,
+            'notify': False,
+            'no_auto_thread': False,
+            'reply_to': False,
+            'record_name': 'a',
+            'model': 'res.partner',
+            'partner_ids': [[6, False, []]],
+            'res_id': 6,
+            'email_from': 'Administrator <admin@example.com>',
+            'subject': 'v15rIke7MpaRhg15FvqRjzet'
+        }
+        model_name = "mail.compose.message"
+        session = ConnectorSession(self.cr, ADMIN_USER_ID, context=context)
+        fc.do_send_mail(session, model_name, mail_vals)
+        model_name = "mail.mail"
+        model_mail = self.registry(model_name)
+        mail_ids = model_mail.search(
+            self.cr, ADMIN_USER_ID,
+            [('subject', '=', 'v15rIke7MpaRhg15FvqRjzet')], context={})
+        self.assertEqual(len(mail_ids) < 1, False,
+                         "Mail Should Have Been Created")
+
+        # Test the content of the mail
+        read_mail = model_mail.browse(self.cr, ADMIN_USER_ID, mail_ids[0])
+        self.assertEqual(read_mail.body, '<p>sample body</p>')
+        self.assertEqual(read_mail.attachment_ids[0].name, "RedHat_spec.doc")
---
+++ asynchronous_batch_mailings/wizard/mail_compose_message.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Authors: Nemry Jonathan
+#    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
+#    All Rights Reserved
+#
+#    WARNING: This program as such is intended to be used by professional
+#    programmers who take the whole responsibility of assessing all potential
+#    consequences resulting from its eventual inadequacies and bugs.
+#    End users who are looking for a ready-to-use solution with commercial
+#    guarantees and support are strongly advised to contact a Free Software
+#    Service Company.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+
+from openerp.osv import orm
+from openerp.tools.translate import _
+
+from openerp.addons.connector.queue.job import job
+from openerp.addons.connector.session import ConnectorSession
+
+_logger = logging.getLogger(__name__)
+
+WORKER_PIVOT = 100
+CHUNK_SIZE = 100
+
+
+class mail_compose_message(orm.TransientModel):
+
+    _inherit = 'mail.compose.message'
+
+    def _prepare_vals(self, vals):
+        vals.pop('id')
+        for key in vals.keys():
+            if(isinstance(vals[key], tuple)):
+                vals[key] = vals[key][0]
+
+    def send_mail(self, cr, uid, ids, context=None):
+        """
+        Send mail by asynchronous way depending parameters
+        """
+        if context is None:
+            context = {}
+        if not context.get('not_async', False):
+            try:
+                parameter_obj = self.pool['ir.config_parameter']
+                worker_pivot = int(parameter_obj.get_param(
+                    cr, uid, 'mail_worker_pivot', WORKER_PIVOT))
+            except:
+                worker_pivot = WORKER_PIVOT
+            if context.get('active_ids') > worker_pivot:
+                res_ids = context.get('active_ids')
+                vals = self.read(cr, uid, ids, [], context=context)[0]
+                self._prepare_vals(vals)
+
+                session = ConnectorSession(cr, uid, context=context)
+                description = _('Send Mail "%s (Chunk Process)') %\
+                    (vals['subject'])
+                prepare_mailings.delay(
+                    session, self._name, vals, res_ids,
+                    description=description, context=context)
+                return
+
+        super(mail_compose_message, self).send_mail(
+            cr, uid, ids, context=context)
+
+
+@job
+def prepare_mailings(session, model_name, vals, active_ids, context=None):
+    self, cr, uid = session, session.cr, session.uid
+
+    ir_param_obj = self.pool['ir.config_parameter']
+    chunck_size = ir_param_obj.get_param(cr, uid, 'job_mail_chunck_size',
+                                         context=context)
+
+    if not chunck_size.isdigit() or int(chunck_size) < 1:
+        chunck_size = CHUNK_SIZE
+        _logger.warning('Could not Retrieve a Valid Value in'
+                        '"job_mail_chunck_size".'
+                        'Chunck Size is %s') % CHUNK_SIZE
+
+    chunck_size = int(chunck_size)
+    chunck_list_active_ids = \
+        [active_ids[i:i + chunck_size] for i in range(0, len(active_ids),
+                                                      chunck_size)]
+
+    i = 1
+    for chunck_active_ids in chunck_list_active_ids:
+        description = _('Send mail "%s" (Mailing %d/%d)') %\
+            (vals['subject'], i, len(chunck_list_active_ids))
+        i = i + 1
+        send_mail.delay(
+            session, model_name, vals, chunck_active_ids,
+            description=description, context=context)
+
+
+@job
+def send_mail(session, model_name, vals, active_ids, context=None):
+    do_send_mail(session, model_name, vals, active_ids, context=context)
+
+
+def do_send_mail(session, model_name, vals, active_ids=None, context=None):
+    """
+    :param vals: contains the values to create a complete mail_compose_message
+        wizard
+    :result: send_mail
+    """
+    if context is None:
+        context = {}
+    self, cr, uid = session, session.cr, session.uid
+    model = session.pool.get(model_name)
+    if active_ids:
+        context['active_ids'] = active_ids
+
+    wiz_id = model.create(self.cr, self.uid, vals, context=context)
+    context['not_async'] = True
+    model.send_mail(cr, uid, [wiz_id], context=context)
