558a3d617df440687902aa84ae1e9077a53fd45c
Olivier LAURENT <olivier.laurent@acsone.eu>
2015-02-20 19:09:40 +0100

    [CHG] ready to publish

--- mozaik/__openerp__.py
+++ mozaik/__openerp__.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#     This file is part of Mozaik, an Odoo module.
+#     This file is part of mozaik, an Odoo module.
 #
 #     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
 #
-#     Mozaik is free software: you can redistribute it and/or
+#     mozaik is free software:
+#     you can redistribute it and/or
 #     modify it under the terms of the GNU Affero General Public License
 #     as published by the Free Software Foundation, either version 3 of
 #     the License, or (at your option) any later version.
 #
-#     Mozaik is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     mozaik is distributed in the hope that it will
+#     be useful but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU Affero General Public License for more details.
 #
 #     You should have received a copy of the
 #     GNU Affero General Public License
-#     along with Mozaik.
+#     along with mozaik.
 #     If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
