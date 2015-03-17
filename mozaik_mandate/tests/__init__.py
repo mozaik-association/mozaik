1acc5be44751f8a40edd6270e380240717d81e9f
Jonathan Nemry <jonathan.nemry@acsone.eu>
2015-03-17 16:38:52 +0100

    [mozaik_mandate] refs #14348 [CHG] * Partner view to manage now two more smart button that counts respectively number of mandates and number of assemblies from a legal partner * res.partner: add two computed fields ext_mandate_count and ext_assembly_count [ADD] * tests for new feature (count mandates and count essemblies)

--- mozaik_mandate/tests/__init__.py
+++ mozaik_mandate/tests/__init__.py
@@ -31,3 +31,4 @@ from . import test_copy_mandate_wizard
 from . import test_electoral_results_wizard
 from . import test_import_candidatures_wizard
 from . import test_update_mandate_end_date_wizard
+from . import test_res_partner
