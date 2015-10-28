1ad13c7e1ef060119e7acc3a8f8a27e60d246df9
Jonathan Nemry (ACSONE) <jonathan.nemry@acsone.eu>
2015-10-28 14:45:34 +0100

    [CHG] reuse native wizard to have a standard way to process print from partner

--- mozaik_address/wizard/print_postal_from_partner_wizard.py
+++ mozaik_address/wizard/print_postal_from_partner_wizard.py
@@ -23,7 +23,7 @@
 #
 ##############################################################################
 
-from openerp import fields, models
+from openerp import models, api
 
 
 class PrintPostalFromPartnerWizard(models.TransientModel):
@@ -31,5 +31,19 @@ class PrintPostalFromPartnerWizard(models.TransientModel):
     _name = 'print.postal.from.partner.wizard'
     _description = 'Print Postal From Partner Wizard'
 
-    export_file = fields.Binary('File', readonly=True)
-    export_filename = fields.Char()
+    @api.multi
+    def print_postal_from_partner_button(self):
+        self.ensure_one()
+        ctx = self.env.context.copy()
+        ctx['active_model'] = 'postal.coordinate'
+        partner_ids = ctx.get('active_ids')
+        domain = [
+            ('is_main', '=', True),
+            ('partner_id', 'in', partner_ids),
+        ]
+        postal_ids = self.env['postal.coordinate'].search(domain)
+        ctx['active_ids'] = postal_ids.ids
+        return self.pool['report'].get_action(
+            self.env.cr, self.env.uid, [],
+            'mozaik_address.report_postal_coordinate_label',
+            data={'report_type': 'qweb-pdf'}, context=ctx)
