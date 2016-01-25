5e523b5272368241ee9e7876cd857dda1e8ff792
Jonathan Nemry (ACSONE) <jonathan.nemry@acsone.eu>
2016-01-25 16:49:54 +0100

    [CHG] closes #17805 * Improve performance of the file generation using a cr.execute instead of the browse exploration (75% faster)

--- mozaik_coordinate/wizard/export_csv.py
+++ mozaik_coordinate/wizard/export_csv.py
@@ -29,9 +29,13 @@ import base64
 from collections import OrderedDict
 
 from openerp.osv import orm, fields
+from openerp.tools.translate import _
 
 from openerp.addons.mozaik_person.res_partner import available_genders, \
     available_tongues
+from export_csv_request import VIRTUAL_TARGET_REQUEST
+from export_csv_request import EMAIL_COORDINATE_REQUEST
+from export_csv_request import POSTAL_COORDINATE_REQUEST
 
 HEADER_ROW = [
     'Internal Identifier',
@@ -43,6 +47,7 @@ HEADER_ROW = [
     'Co-residency Line 1',
     'Co-residency Line 2',
     'Internal Instance',
+    'Power Level',
     'Reference',
     'Birthdate',
     'Gender',
@@ -97,119 +102,103 @@ class export_csv(orm.TransientModel):
         Get the values of the specified obj, which should be an instance
         of the specified model, either an email or a postal coordinate.
         """
-
-        def safe_get(o, attr, default=None):
-            try:
-                return getattr(o, attr)
-            except orm.except_orm:
-                return default
-
         def _get_utf8(data):
             if not data:
                 return None
-            return data.encode('utf-8')
-
-        # Test access coordinate (VIP READER)
-        partner = safe_get(obj, 'partner_id')
-
-        if not partner:
-            return False
-
-        if model == 'postal.coordinate':
-            pc = obj
-            # for postal coordinates, get email coordinate from the partner
-            ec = partner.email_coordinate_id or None
-        elif model == 'email.coordinate':
-            # for email coordinates, get postal coordinate from the partner
-            pc = partner.postal_coordinate_id or None
-            ec = obj
-        elif model == 'virtual.target':
-            # safe evaluation to avoid VIP security warning
-            pc = safe_get(obj.postal_coordinate_id, 'address_id') and \
-                obj.postal_coordinate_id or partner.postal_coordinate_id or \
-                None
-            ec = safe_get(obj.email_coordinate_id, 'email') and \
-                obj.email_coordinate_id or partner.email_coordinate_id or \
-                None
-
-        xc = partner.fix_coordinate_id or None
-        mc = partner.mobile_coordinate_id or None
-        fc = partner.fax_coordinate_id or None
-
-        cc = pc and pc.co_residency_id or None
-        ic = partner.int_instance_id
+            return unicode(data).encode('utf-8', 'ignore')
 
         export_values = OrderedDict([
-            ('identifier', partner.identifier or None),
-            ('name', _get_utf8(partner.name)),
-            ('lastname', _get_utf8(partner.lastname)),
-            ('firstname', _get_utf8(partner.firstname)),
-            ('usual_lastname', _get_utf8(partner.usual_lastname)),
-            ('usual_firstname', _get_utf8(partner.usual_firstname)),
-            ('printable_name', cc and _get_utf8(cc.line) or
-                _get_utf8(partner.printable_name)),
-            ('co_residency', cc and _get_utf8(cc.line2)),
-            ('instance', ic and _get_utf8(ic.display_name)),
-            ('reference', _get_utf8(partner.reference)),
-            ('birth_date', partner.birth_date or None),
-            ('gender', available_genders.get(partner.gender, None)),
-            ('tongue', available_tongues.get(partner.tongue, None)),
-            ('adr_main', pc and pc.is_main or False),
-            ('adr_unauthorized', pc and pc.unauthorized or False),
-            ('adr_vip', pc and pc.vip or False),
-            ('street2', pc and _get_utf8(pc.address_id.street2)),
-            ('street', pc and _get_utf8(pc.address_id.street)),
-            ('zip', pc and pc.address_id.zip),
-            ('city', pc and _get_utf8(pc.address_id.city)),
-            ('country_code', pc and pc.address_id.country_code),
-            ('country_name', pc and _get_utf8(pc.address_id.country_id.name)),
-            ('fix_main', xc and xc.is_main or False),
-            ('fix_unauthorized', xc and xc.unauthorized or False),
-            ('fix_vip', xc and xc.vip or False),
-            ('fix', xc and _get_utf8(xc.phone_id.name)),
-            ('mobile_main', mc and mc.is_main or False),
-            ('mobile_unauthorized', mc and mc.unauthorized or False),
-            ('mobile_vip', mc and mc.vip or False),
-            ('mobile', mc and _get_utf8(mc.phone_id.name)),
-            ('fax_main', fc and fc.is_main or False),
-            ('fax_unauthorized', fc and fc.unauthorized or False),
-            ('fax_vip', fc and fc.vip or False),
-            ('fax', fc and _get_utf8(fc.phone_id.name)),
-            ('email_main', ec and ec.is_main or False),
-            ('email_unauthorized', ec and ec.unauthorized or False),
-            ('email_vip', ec and ec.vip or False),
-            ('email', ec and _get_utf8(ec.email)),
-            ('website', _get_utf8(partner.website)),
-            ('secondary_website', _get_utf8(partner.secondary_website)),
+            ('identifier', obj.get('identifier')),
+            ('name', _get_utf8(obj.get('name'))),
+            ('lastname', _get_utf8(obj.get('lastname'))),
+            ('firstname', _get_utf8(obj.get('firstname'))),
+            ('usual_lastname', _get_utf8(obj.get('usual_lastname'))),
+            ('usual_firstname', _get_utf8(obj.get('usual_firstname'))),
+            ('printable_name', _get_utf8(obj.get('printable_name'))),
+            ('co_residency', _get_utf8(obj.get('co_residency'))),
+            ('instance', _get_utf8(obj.get('instance'))),
+            ('power_name', _get_utf8(obj.get('power_name'))),
+            ('reference', _get_utf8(obj.get('reference'))),
+            ('birth_date', obj.get('birth_date')),
+            ('gender', available_genders.get(obj.get('gender'))),
+            ('tongue', available_tongues.get(obj.get('tongue'))),
+            ('adr_main', obj.get('adr_main')),
+            ('adr_unauthorized', obj.get('adr_unauthorized')),
+            ('adr_vip', obj.get('adr_vip')),
+            ('street2', _get_utf8(obj.get('street2'))),
+            ('street', _get_utf8(obj.get('street'))),
+            ('zip', obj.get('zip')),
+            ('city', _get_utf8(obj.get('city'))),
+            ('country_code', obj.get('country_code')),
+            ('country_name', _get_utf8(obj.get('country_name'))),
+            ('fix_main', obj.get('fix_main')),
+            ('fix_unauthorized', obj.get('fix_unauthorized')),
+            ('fix_vip', obj.get('fix_vip')),
+            ('fix', _get_utf8(obj.get('fix'))),
+            ('mobile_main', obj.get('mobile_main')),
+            ('mobile_unauthorized', obj.get('mobile_unauthorized')),
+            ('mobile_vip', obj.get('mobile_vip')),
+            ('mobile', _get_utf8(obj.get('mobile'))),
+            ('fax_main', obj.get('fax_main')),
+            ('fax_unauthorized', obj.get('fax_unauthorized')),
+            ('fax_vip', obj.get('fax_vip')),
+            ('fax', _get_utf8(obj.get('fax'))),
+            ('email_main', obj.get('email_main')),
+            ('email_unauthorized', obj.get('email_unauthorized')),
+            ('email_vip', obj.get('email_vip')),
+            ('email', _get_utf8(obj.get('email'))),
+            ('website', _get_utf8(obj.get('website'))),
+            ('secondary_website', _get_utf8(obj.get('secondary_website'))),
         ])
         return export_values
+
+    def _prefetch_csv_datas(self, cr, uid, model, model_ids, context=None):
+        if not model_ids:
+            return
+        if model == 'email.coordinate':
+            query = """
+            %s WHERE ec.id IN %%s
+            """ % EMAIL_COORDINATE_REQUEST
+        elif model == 'postal.coordinate':
+            query = """
+            %s WHERE pc.id IN %%s
+            """ % POSTAL_COORDINATE_REQUEST
+        elif model == 'virtual.target':
+            query = """
+            %s WHERE vt.id IN %%s
+            """ % VIRTUAL_TARGET_REQUEST
+        else:
+            raise orm.except_orm(
+                _('Error'),
+                _('Model %s Not supported for csv generation!') % model)
+        cr.execute(query, (tuple(model_ids),))
+        for row in cr.dictfetchall():
+            yield row
 
     def get_csv(self, cr, uid, model, model_ids, group_by=False, context=None):
         """
         Build a CSV file related to a coordinate model
         """
-
-        objects = self.pool[model].browse(cr, uid, model_ids, context=context)
-        tmp = tempfile.NamedTemporaryFile(prefix='Extract', suffix=".csv",
-                                          delete=False)
+        tmp = tempfile.NamedTemporaryFile(
+            prefix='Extract', suffix=".csv", delete=False)
         f = open(tmp.name, "r+")
         writer = csv.writer(f)
         writer.writerow(self.get_csv_rows(cr, uid, model, context=context))
         co_residencies = []
-        for obj in objects:
-            if model == 'postal.coordinate':
-                # when grouping by co_residency, output only one row
-                # by co_residency
-                if group_by and obj.co_residency_id and \
-                   obj.co_residency_id.id in co_residencies:
+        if model_ids:
+            for data in self._prefetch_csv_datas(
+                    cr, uid, model, model_ids, context=context):
+                if model == 'postal.coordinate':
+                    # when grouping by co_residency, output only one row
+                    # by co_residency
+                    if group_by and data.get(
+                            'co_residency_id') in co_residencies:
+                        continue
+                    co_residencies.append(data.get('co_residency_id'))
+                export_values = self.get_csv_values(cr, uid, model, data)
+                if not export_values:
                     continue
-                co_residencies.append(obj.co_residency_id.id)
-
-            export_values = self.get_csv_values(cr, uid, model, obj)
-            if not export_values:
-                continue
-
-            writer.writerow(export_values.values())
+                writer.writerow(export_values.values())
         f.close()
         f = open(tmp.name, "r")
         csv_content = f.read()
---
+++ mozaik_coordinate/wizard/export_csv_request.py
@@ -0,0 +1,242 @@
+COMMON_REQUEST = """
+SELECT
+ p.identifier,
+ p.name,
+ p.lastname,
+ p.firstname,
+ p.usual_lastname,
+ p.usual_firstname,
+ p.reference,
+ p.birth_date,
+ p.gender,
+ p.tongue,
+ p.website,
+ p.secondary_website,
+ CASE
+  WHEN cc IS NOT NULL
+  THEN cc.line
+  ELSE p.printable_name
+ END AS printable_name,
+ cc.id as co_residency_id,
+ cc.line2 as co_residency,
+ instance.name as instance,
+ ipl.name as power_name,
+ pc.is_main as adr_main,
+ pc.unauthorized as adr_unauthorized,
+ pc.vip as adr_vip,
+ CASE
+  WHEN pc.vip = True
+  THEN 'VIP'
+  ELSE address.street2
+ END AS street2,
+ CASE
+  WHEN pc.vip = True
+  THEN 'VIP'
+  ELSE address.street
+ END AS street,
+ CASE
+  WHEN pc.vip = True
+  THEN 'VIP'
+  ELSE address.zip
+ END AS zip,
+ CASE
+  WHEN pc.vip = True
+  THEN 'VIP'
+  ELSE address.city
+ END AS city,
+ CASE
+  WHEN pc.vip = True
+  THEN 'VIP'
+  ELSE country.name
+ END AS country_name,
+ CASE
+  WHEN pc.vip = True
+  THEN 'VIP'
+  ELSE country.code
+ END AS country_code,
+ fix.is_main as fix_main,
+ fix.unauthorized as fix_unauthorized,
+ fix.vip as fix_vip,
+ CASE
+  WHEN fix.vip = True
+  THEN 'VIP'
+  ELSE fix_phone.name
+ END AS fix,
+ mobile.is_main as mobile_main,
+ mobile.unauthorized as mobile_unauthorized,
+ mobile.vip as mobile_vip,
+ CASE
+  WHEN mobile.vip = True
+  THEN 'VIP'
+  ELSE mobile_phone.name
+ END AS mobile,
+ fax.is_main as fax_main,
+ fax.unauthorized as fax_unauthorized,
+ fax.vip as fax_vip,
+ CASE
+  WHEN mobile.vip = True
+  THEN 'VIP'
+  ELSE fax_phone.name
+ END AS fax,
+ ec.is_main as email_main,
+ ec.unauthorized as email_unauthorized,
+ ec.vip as email_vip,
+ ec.email
+"""
+VIRTUAL_TARGET_REQUEST = """
+%s
+FROM
+virtual_target vt
+
+JOIN res_partner p
+ ON p.id = vt.partner_id
+
+LEFT OUTER JOIN int_instance instance
+ ON instance.id = p.int_instance_id
+
+LEFT OUTER JOIN int_power_level ipl
+ ON instance.power_level_id = ipl.id
+
+LEFT OUTER JOIN postal_coordinate pc
+ ON pc.id = vt.postal_coordinate_id
+
+LEFT OUTER JOIN address_address address
+ ON address.id = pc.address_id
+
+LEFT OUTER JOIN res_country country
+ ON country.id = address.country_id
+
+LEFT OUTER JOIN co_residency cc
+ ON cc.id = pc.co_residency_id
+
+LEFT OUTER JOIN phone_coordinate fix
+ ON fix.partner_id = p.id AND
+ fix.is_main = True AND
+ fix.coordinate_type= 'fix'
+
+LEFT OUTER JOIN phone_phone fix_phone
+ ON fix_phone.id = fix.id
+
+LEFT OUTER JOIN phone_coordinate mobile
+ ON mobile.partner_id = p.id AND
+ mobile.is_main = True AND
+ mobile.coordinate_type = 'mobile'
+
+LEFT OUTER JOIN phone_phone mobile_phone
+ ON mobile_phone.id = mobile.id
+
+LEFT OUTER JOIN phone_coordinate fax
+ ON fax.partner_id = p.id AND
+ fax.is_main = True AND
+ fax.coordinate_type = 'fix'
+
+LEFT OUTER JOIN phone_phone fax_phone
+ ON fax_phone.id = fax.id
+
+LEFT OUTER JOIN email_coordinate ec
+ ON ec.id = vt.postal_coordinate_id
+""" % COMMON_REQUEST
+
+EMAIL_COORDINATE_REQUEST = """
+%s
+FROM email_coordinate ec
+
+JOIN res_partner p
+ ON p.id = ec.partner_id
+
+LEFT OUTER JOIN int_instance instance
+ ON instance.id = p.int_instance_id
+
+LEFT OUTER JOIN int_power_level ipl
+ ON instance.power_level_id = ipl.id
+
+LEFT OUTER JOIN postal_coordinate pc
+ ON pc.partner_id = p.id AND
+ pc.is_main = True
+
+LEFT OUTER JOIN address_address address
+ ON address.id = pc.address_id
+
+LEFT OUTER JOIN res_country country
+ ON country.id = address.country_id
+
+LEFT OUTER JOIN co_residency cc
+ ON cc.id = pc.co_residency_id
+
+LEFT OUTER JOIN phone_coordinate fix
+ ON fix.partner_id = p.id AND
+ fix.is_main = True AND
+ fix.coordinate_type= 'fix'
+
+LEFT OUTER JOIN phone_phone fix_phone
+ ON fix_phone.id = fix.id
+
+LEFT OUTER JOIN phone_coordinate mobile
+ ON mobile.partner_id = p.id AND
+ mobile.is_main = True AND
+ mobile.coordinate_type = 'mobile'
+
+LEFT OUTER JOIN phone_phone mobile_phone
+ ON mobile_phone.id = mobile.id
+
+LEFT OUTER JOIN phone_coordinate fax
+ ON fax.partner_id = p.id AND
+ fax.is_main = True AND
+ fax.coordinate_type = 'fix'
+
+LEFT OUTER JOIN phone_phone fax_phone
+ ON fax_phone.id = fax.id
+""" % COMMON_REQUEST
+
+POSTAL_COORDINATE_REQUEST = """
+%s
+
+FROM postal_coordinate pc
+
+JOIN res_partner p
+ ON p.id = pc.partner_id
+
+LEFT OUTER JOIN int_instance instance
+ ON instance.id = p.int_instance_id
+
+LEFT OUTER JOIN int_power_level ipl
+ ON instance.power_level_id = ipl.id
+
+LEFT OUTER JOIN address_address address
+ ON address.id = pc.address_id
+
+LEFT OUTER JOIN res_country country
+ ON country.id = address.country_id
+
+LEFT OUTER JOIN co_residency cc
+ ON cc.id = pc.co_residency_id
+
+LEFT OUTER JOIN phone_coordinate fix
+ ON fix.partner_id = p.id AND
+ fix.is_main = True AND
+ fix.coordinate_type= 'fix'
+
+LEFT OUTER JOIN phone_phone fix_phone
+ ON fix_phone.id = fix.id
+
+LEFT OUTER JOIN phone_coordinate mobile
+ ON mobile.partner_id = p.id AND
+ mobile.is_main = True AND
+ mobile.coordinate_type = 'mobile'
+
+LEFT OUTER JOIN phone_phone mobile_phone
+ ON mobile_phone.id = mobile.id
+
+LEFT OUTER JOIN phone_coordinate fax
+ ON fax.partner_id = p.id AND
+ fax.is_main = True AND
+ fax.coordinate_type = 'fix'
+
+LEFT OUTER JOIN phone_phone fax_phone
+ ON fax_phone.id = fax.id
+
+LEFT OUTER JOIN email_coordinate ec
+ ON ec.partner_id = p.id AND
+ ec.is_main = True
+
+""" % COMMON_REQUEST
