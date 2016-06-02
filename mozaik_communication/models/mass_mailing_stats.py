a02db2c3a2a0aa59bddea9ac1321de9dbb1f0068
Olivier LAURENT <olivier.laurent@acsone.eu>
2016-06-02 14:08:18 +0200

    [CHG] make email_coordinate_id on statistic a stored field [FIX] dependency issue refs #17440

--- mozaik_communication/models/mass_mailing_stats.py
+++ mozaik_communication/models/mass_mailing_stats.py
@@ -12,12 +12,22 @@ class MailMailStats(models.Model):
     @api.multi
     @api.depends('res_id', 'model')
     def _compute_email_coordinate_id(self):
+        '''
+        Transform res_id integers to real email coordinates ids
+        '''
+        coord_ids = [
+            stat.res_id for stat in self
+            if stat.model == 'email.coordinate' and stat.res_id
+        ]
+        coords = self.env['email.coordinate'].search(
+            [('id', 'in', coord_ids)])
         for stat in self:
-            if stat.model == 'email.coordinate' and stat.res_id:
+            if stat.res_id in coords.ids:
                 stat.email_coordinate_id = stat.res_id
             else:
                 stat.email_coordinate_id = False
 
     email_coordinate_id = fields.Many2one(
         comodel_name='email.coordinate', string='Email Coordinate',
-        compute='_compute_email_coordinate_id')
+        compute='_compute_email_coordinate_id', store=True,
+        ondelete='cascade')
