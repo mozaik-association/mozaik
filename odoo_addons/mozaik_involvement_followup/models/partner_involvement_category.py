638265d6c888690eb94a5c0b30570025f292247a
Olivier-LAURENT <olivier.laurent@acsone.eu>
2018-01-31 16:14:10 +0100

    [CHG] add follow-up categories m2m on category, refs #27036

--- odoo_addons/mozaik_involvement_followup/models/partner_involvement_category.py
+++ odoo_addons/mozaik_involvement_followup/models/partner_involvement_category.py
@@ -2,7 +2,8 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from openerp import _, api, fields, models
+from openerp.exceptions import ValidationError
 
 
 class PartnerInvolvementCategory(models.Model):
@@ -19,6 +20,20 @@ class PartnerInvolvementCategory(models.Model):
         domain=[('type', '=', 'int')],
         index=True, track_visibility='onchange')
 
+    involvement_category_ids = fields.Many2many(
+        'partner.involvement.category',
+        relation='partner_involvement_category_followup_rel',
+        column1='partner_id', column2='followup_category_id',
+        string='Follow-up Categories',
+        domain=[('involvement_type', '=', False)])
+
+    parent_involvement_category_ids = fields.Many2many(
+        'partner.involvement.category',
+        relation='partner_involvement_category_followup_rel',
+        column2='partner_id', column1='followup_category_id',
+        string='Parent Categories',
+        help='Categories specifying this category as follow-up category')
+
     _sql_constraints = [
         (
             'nb_deadline_days_no_negative',
@@ -32,10 +47,35 @@ class PartnerInvolvementCategory(models.Model):
         ),
     ]
 
+    @api.constrains('involvement_category_ids')
+    def _check_involvement_category_ids_recursion(self):
+        """
+        :raise ValidationError: when a recursion is detected
+        """
+        if not self._check_m2m_recursion('involvement_category_ids'):
+            raise ValidationError(
+                _('Error! You cannot specify '
+                  'recursive set of follow-up categories.'))
+        return True
+
+    @api.constrains('involvement_type')
+    def _check_involvement_category_followup_without_type(self):
+        """
+        :raise ValidationError: when a typed category is used
+                                as followup category
+        """
+        for ic in self.filtered(lambda s: s.involvement_type):
+            if ic.parent_involvement_category_ids:
+                raise ValidationError(
+                    _('Error! You cannot specify '
+                      'an involvement type on followup categories'))
+        return True
+
     @api.onchange('nb_deadline_days')
     def _onchange_nb_deadline_days(self):
         if not self.nb_deadline_days:
             self.mandate_category_id = False
+            self.involvement_category_ids = False
 
     @api.model
     def read_followers_data(self, follower_ids):
@@ -52,3 +92,26 @@ class PartnerInvolvementCategory(models.Model):
             res[2]['is_editable'] = is_editable
             res[2]['is_uid'] = uid == res[0]
         return result
+
+    @api.model
+    @api.returns('self', lambda value: value.id)
+    def create(self, vals):
+        '''
+        Protect use of followup categories delegating rules check
+        to the specified followup categories (mode=write)
+        '''
+        res = super(PartnerInvolvementCategory, self).create(vals)
+        if vals.get('involvement_category_ids'):
+            res.involvement_category_ids.check_access_rule('write')
+        return res
+
+    @api.multi
+    def write(self, vals):
+        '''
+        Protect use of followup categories delegating rules check
+        to the specified followup categories (mode=write)
+        '''
+        res = super(PartnerInvolvementCategory, self).write(vals)
+        if vals.get('involvement_category_ids'):
+            self.mapped('involvement_category_ids').check_access_rule('write')
+        return res
