8e8fca91b58c2f2aa865ad74295e06fcf9356d2a
andreparames <github@andreparames.com>
2018-03-06 09:24:01 +0100

    [CHG] Add "save as template" to mass function
    Implements essentially the same feature as in Odoo's mail composer,
    by creating an email.template from the subject/body and then
    linking the current message to it. refs #28189

--- odoo_addons/mozaik_communication/wizard/__init__.py
+++ odoo_addons/mozaik_communication/wizard/__init__.py
@@ -24,6 +24,7 @@
 ##############################################################################
 
 from . import distribution_list_mass_function
+from . import distribution_list_mass_function_v8
 from . import mail_compose_message
 from . import add_registration
 from . import export_csv_request
