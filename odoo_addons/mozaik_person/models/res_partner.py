95a6828fd6a22269d35a83cd93175ec15b60c3d2
andreparames <github@andreparames.com>
2018-04-06 18:23:18 +0200

    [CHG] Add booleans to identify donors and volunteers
    refs #28742

--- odoo_addons/mozaik_person/models/res_partner.py
+++ odoo_addons/mozaik_person/models/res_partner.py
@@ -2,7 +2,7 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from openerp import fields, models, api
 
 
 class ResPartner(models.Model):
@@ -10,3 +10,22 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     nationality_id = fields.Many2one(track_visibility='onchange')
+    is_donor = fields.Boolean(
+        string="Is a donor",
+        compute='_compute_involvement_bools', store=True)
+    is_volunteer = fields.Boolean(
+        string="Is a volunteer",
+        compute='_compute_involvement_bools', store=True)
+
+    @api.multi
+    @api.depends(
+        'partner_involvement_ids',
+        'partner_involvement_ids.active',
+        'partner_involvement_inactive_ids',
+        'partner_involvement_inactive_ids.active',
+    )
+    def _compute_involvement_bools(self):
+        for partner in self:
+            types = partner.partner_involvement_ids.mapped('involvement_type')
+            partner.is_donor = 'donation' in types
+            partner.is_volunteer = 'voluntary' in types
