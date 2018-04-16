c7511dc358c28508eddb6fb837303a92766dc466
andreparames <github@andreparames.com>
2018-04-16 11:59:05 +0200

    [FIX] Don't accept multiple records when returning only one

--- odoo_addons/mozaik_communication/wizard/distribution_list_mass_function_v8.py
+++ odoo_addons/mozaik_communication/wizard/distribution_list_mass_function_v8.py
@@ -9,26 +9,26 @@ class DistributionListMassFunction(models.TransientModel):
 
     @api.multi
     def save_as_template(self):
+        self.ensure_one()
         template_name = u"Mass Function: {subject}"
         model = self.env['ir.model'].search(
             [('model', '=', 'email.coordinate')], limit=1)
-        for record in self:
-            values = {
-                'name': template_name.format(subject=record.subject),
-                'subject': record.subject or False,
-                'body_html': record.body or False,
-                'model_id': model.id,
-            }
-            template = self.env['email.template'].create(values)
-            new_values = record.onchange_template_id(template.id)['value']
-            new_values['email_template_id'] = template.id
-            record.write(new_values)
+        values = {
+            'name': template_name.format(subject=self.subject),
+            'subject': self.subject or False,
+            'body_html': self.body or False,
+            'model_id': model.id,
+        }
+        template = self.env['email.template'].create(values)
+        new_values = self.onchange_template_id(template.id)['value']
+        new_values['email_template_id'] = template.id
+        self.write(new_values)
 
         return {
             'type': 'ir.actions.act_window',
             'view_mode': 'form',
             'view_type': 'form',
-            'res_id': record.id,
+            'res_id': self.id,
             'res_model': self._name,
             'target': 'new',
             'context': dict(self.env.context),
