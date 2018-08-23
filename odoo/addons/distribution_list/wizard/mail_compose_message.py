a247bdff17809bd3e324d94f51b0acfdbb4021b5
François Honoré <francois.honore@acsone.eu>
2018-08-23 09:52:01 +0200

    [MIG][11.0] Migration of distribution_list (+flake/pylint and unit test)

---
+++ odoo/addons/distribution_list/wizard/mail_compose_message.py
@@ -0,0 +1,52 @@
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, models, fields
+from odoo.fields import first
+
+
+class MailComposeMessage(models.TransientModel):
+    _inherit = 'mail.compose.message'
+
+    distribution_list_id = fields.Many2one(
+        "distribution.list",
+        "Distribution list",
+    )
+
+    @api.model
+    def create(self, vals):
+        """
+        This override allows the user to force the mass mail to
+        the distribution list even if the header check-box was checked
+        :param vals: dict
+        :return: self recordset
+        """
+        context = self.env.context.copy()
+        if 'distribution_list_id' in vals and 'active_domain' in context:
+            context.pop('active_domain')
+            if vals.get('use_active_domain'):
+                vals.update({
+                    'use_active_domain': False,
+                    'composition_mode': 'mass_mail',
+                })
+
+        return super(
+            MailComposeMessage, self.with_context(context)).create(vals)
+
+    @api.multi
+    def send_mail(self):
+        """
+        Overriding of send mail: it has to compute the ids
+        of the distribution list to send mail.
+        :return: super result
+        """
+        context = self.env.context.copy()
+        dl_computed = context.get('dl_computed')
+        dist_list = self.distribution_list_id
+        if dist_list and not dl_computed:
+            mains, _ = dist_list._get_complex_distribution_list_ids()
+            context.update({
+                'active_ids': mains.ids,
+                'active_model': first(mains)._name,
+            })
+        return super(
+            MailComposeMessage, self.with_context(context)).send_mail()
---
+++ odoo/addons/distribution_list/wizard/merge_distribution_list.py
@@ -0,0 +1,33 @@
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, models, exceptions, fields, _
+
+
+class MergeDistributionList(models.TransientModel):
+    """
+    Wizard to merge one distribution list into selected distribution lists
+    """
+    _name = 'merge.distribution.list'
+    _description = 'Complete Distribution List'
+
+    distribution_list_id = fields.Many2one(
+        "distribution.list",
+        "Distribution list",
+        help="Distribution list to complete",
+        required=True,
+    )
+
+    @api.multi
+    def _merge_distribution_list(self):
+        """
+        Merge selected distribution list (lines) into the distribution list
+        selected into the wizard
+        :return: dict/action
+        """
+        self.ensure_one()
+        active_ids = self.env.context.get('active_ids', [])
+        if not active_ids:
+            raise exceptions.UserError(
+                _("Please select at least one Distribution List!"))
+        self.distribution_list_id._complete_distribution_list(active_ids)
+        return {}
