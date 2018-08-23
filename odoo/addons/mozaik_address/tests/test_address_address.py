cf9260416eb9465e2c2a285ffbeb0c3a60209687
Denis Robinet (ACSONE) <denis.robinet@acsone.eu>
2018-08-23 17:08:07 +0200

    [MIG] Update migrate.txt

--- odoo/addons/mozaik_address/tests/test_address_address.py
+++ odoo/addons/mozaik_address/tests/test_address_address.py
@@ -1,26 +1,5 @@
-##############################################################################
-#
-#     This file is part of mozaik_address, an Odoo module.
-#
-#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
-#
-#     mozaik_address is free software:
-#     you can redistribute it and/or
-#     modify it under the terms of the GNU Affero General Public License
-#     as published by the Free Software Foundation, either version 3 of
-#     the License, or (at your option) any later version.
-#
-#     mozaik_address is distributed in the hope that it will
-#     be useful but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU Affero General Public License for more details.
-#
-#     You should have received a copy of the
-#     GNU Affero General Public License
-#     along with mozaik_address.
-#     If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo.tests.common import TransactionCase
 from odoo.exceptions import ValidationError
 
