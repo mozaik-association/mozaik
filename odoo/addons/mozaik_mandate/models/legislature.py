f82fa7c2747ab9db68c0f008c9506a0d482f67e8
Olivier-LAURENT <olivier.laurent@acsone.eu>
2019-11-11 18:36:33 +0100

    [FIX] wrong new api migration

--- odoo/addons/mozaik_mandate/models/legislature.py
+++ odoo/addons/mozaik_mandate/models/legislature.py
@@ -1,9 +1,6 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from datetime import datetime
-from odoo import api, fields, models, _
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT, \
-    DEFAULT_SERVER_DATETIME_FORMAT
+from odoo import api, models, _
 from odoo.exceptions import ValidationError
 
 
@@ -17,17 +14,14 @@ class Legislature(models.Model):
         if new_deadline_date:
             for legis in self:
                 if legis.deadline_date != new_deadline_date:
-                    if (datetime.strptime(new_deadline_date,
-                                          DEFAULT_SERVER_DATE_FORMAT) <
-                        datetime.strptime(fields.datetime.now(),
-                                          DEFAULT_SERVER_DATETIME_FORMAT)):
+                    if new_deadline_date < legis.deadline_date:
                         raise ValidationError(
                             _('New deadline date must be greater or'
-                              ' equal than today !'))
-                    mandate_obj = self.pool.get('sta.mandate')
-                    mandate = mandate_obj.search(
+                              ' equal than the previous deadline date !'))
+                    mandate_obj = self.env['sta.mandate']
+                    mandates = mandate_obj.search(
                         [('legislature_id', '=', legis.id),
                          ('deadline_date', '>', new_deadline_date)])
-                    if mandate:
-                        mandate.write({'deadline_date': new_deadline_date})
+                    if mandates:
+                        mandates.write({'deadline_date': new_deadline_date})
         return super().write(vals)
