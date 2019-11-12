2ca75807186bb30de507b8d5f777ad82b4b61bff
Olivier-LAURENT <olivier.laurent@acsone.eu>
2019-11-13 00:30:58 +0100

    [CHG] add a real display_name to the model

--- odoo/addons/mozaik_mandate/models/generic_mandate.py
+++ odoo/addons/mozaik_mandate/models/generic_mandate.py
@@ -9,7 +9,6 @@ class GenericMandate(models.Model):
     _name = "generic.mandate"
     _description = 'Generic Mandate'
     _auto = False
-    _rec_name = 'mandate_ref'
     _order = 'partner_id, mandate_category_id'
 
     _discriminant_field = 'partner_id'
@@ -22,8 +21,7 @@ class GenericMandate(models.Model):
         :return: str, int, float, bool
         """
         self.ensure_one()
-        model, m_id = self.mandate_ref.split(',')
-        return self.env[self.model].browse(int(m_id))\
+        return self.env[self.model].browse(self.mandate_id)\
             ._get_discriminant_value(force_field=force_field)
 
     def _is_discriminant_m2o(self):
@@ -52,12 +50,10 @@ class GenericMandate(models.Model):
     is_duplicate_allowed = fields.Boolean(
         string='Allowed Incompatible Mandate')
 
-    def _select_int_mandate(self):
+    def _select_mandate(self):
         return """
-        'int.mandate' AS model,
         mandate.unique_id as id,
         mandate.id as mandate_id,
-        concat('int.mandate,', mandate.id) as mandate_ref,
         mandate.mandate_category_id,
         mandate.partner_id,
         mandate.start_date,
@@ -66,72 +62,58 @@ class GenericMandate(models.Model):
         mandate.is_duplicate_allowed,
         partner.name as assembly_name
         """
+
+    def _select_int_mandate(self):
+        return """
+        'int.mandate' AS model,
+        concat('int.mandate,', mandate.id) as mandate_ref,""" + \
+            self._select_mandate()
 
     def _select_ext_mandate(self):
         return """
         'ext.mandate' AS model,
-        mandate.unique_id as id,
-        mandate.id as mandate_id,
-        concat('ext.mandate,', mandate.id),
-        mandate.mandate_category_id,
-        mandate.partner_id,
-        mandate.start_date,
-        mandate.deadline_date,
-        mandate.is_duplicate_detected,
-        mandate.is_duplicate_allowed,
-        partner.name as assembly_name
-        """
+        concat('ext.mandate,', mandate.id),""" + self._select_mandate()
 
     def _select_sta_mandate(self):
         return """
         'sta.mandate' AS model,
-        mandate.unique_id as id,
-        mandate.id as mandate_id,
-        concat('sta.mandate,', mandate.id),
-        mandate.mandate_category_id,
-        mandate.partner_id,
-        mandate.start_date,
-        mandate.deadline_date,
-        mandate.is_duplicate_detected,
-        mandate.is_duplicate_allowed,
-        partner.name as assembly_name
-        """
+        concat('sta.mandate,', mandate.id),""" + self._select_mandate()
 
     def init(self):
         tools.drop_view_if_exists(self._cr, 'generic_mandate')
         self.env.cr.execute("""
             create or replace view generic_mandate as (
-                    SELECT %(select_int_mandate)s
-                        FROM int_mandate  AS mandate
-                        JOIN int_assembly AS assembly
-                          ON assembly.id = mandate.int_assembly_id
-                        JOIN res_partner  AS partner
-                          ON partner.id = assembly.partner_id
-                        WHERE mandate.active = True
-                        AND mandate.end_date is NULL
+                SELECT %(select_int_mandate)s
+                    FROM int_mandate  AS mandate
+                    JOIN int_assembly AS assembly
+                      ON assembly.id = mandate.int_assembly_id
+                    JOIN res_partner  AS partner
+                      ON partner.id = assembly.partner_id
+                    WHERE mandate.active = True
+                    AND mandate.end_date is NULL
 
-                    UNION
+                UNION
 
-                    SELECT %(select_sta_mandate)s
-                        FROM sta_mandate  AS mandate
-                        JOIN sta_assembly AS assembly
-                          ON assembly.id = mandate.sta_assembly_id
-                        JOIN res_partner  AS partner
-                          ON partner.id = assembly.partner_id
-                        WHERE mandate.active = True
-                        AND mandate.end_date is NULL
+                SELECT %(select_sta_mandate)s
+                    FROM sta_mandate  AS mandate
+                    JOIN sta_assembly AS assembly
+                      ON assembly.id = mandate.sta_assembly_id
+                    JOIN res_partner  AS partner
+                      ON partner.id = assembly.partner_id
+                    WHERE mandate.active = True
+                    AND mandate.end_date is NULL
 
-                    UNION
+                UNION
 
-                    SELECT %(select_ext_mandate)s
-                        FROM ext_mandate  AS mandate
-                        JOIN ext_assembly AS assembly
-                          ON assembly.id = mandate.ext_assembly_id
-                        JOIN res_partner  AS partner
-                          ON partner.id = assembly.partner_id
-                        WHERE mandate.active = True
-                        AND mandate.end_date is NULL
-                )
+                SELECT %(select_ext_mandate)s
+                    FROM ext_mandate  AS mandate
+                    JOIN ext_assembly AS assembly
+                      ON assembly.id = mandate.ext_assembly_id
+                    JOIN res_partner  AS partner
+                      ON partner.id = assembly.partner_id
+                    WHERE mandate.active = True
+                    AND mandate.end_date is NULL
+            )
             """, {
             "select_int_mandate": AsIs(self._select_int_mandate()),
             "select_sta_mandate": AsIs(self._select_sta_mandate()),
@@ -139,10 +121,20 @@ class GenericMandate(models.Model):
         })
 
     @api.multi
+    def name_get(self):
+        res = []
+        for mandate in self:
+            display_name = '{name} ({mandate_category})'.format(
+                name=mandate.partner_id.name,
+                mandate_category=mandate.mandate_category_id.name)
+            res.append((mandate.id, display_name))
+        return res
+
+    @api.multi
     def button_view_mandate(self):
         """
         View mandates in its form view depending on model
         """
         self.ensure_one()
-        model, m_id = self.mandate_ref.split(',')
-        return self.env[self.model].browse(int(m_id)).get_formview_action()
+        return self.env[self.model].browse(
+            self.mandate_id).get_formview_action()
