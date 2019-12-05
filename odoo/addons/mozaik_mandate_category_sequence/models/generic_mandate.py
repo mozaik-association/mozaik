52af4ac3104ea8b99d803d6abd2b9a45d0d6a0d7
Olivier-LAURENT <olivier.laurent@acsone.eu>
2019-12-05 18:11:58 +0100

    [TRS] refs #37912

--- odoo/addons/mozaik_mandate_category_sequence/models/generic_mandate.py
+++ odoo/addons/mozaik_mandate_category_sequence/models/generic_mandate.py
@@ -1,7 +1,7 @@
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class GenericMandate(models.Model):
@@ -9,7 +9,7 @@ class GenericMandate(models.Model):
     _inherit = 'generic.mandate'
     _order = 'partner_id, sequence, mandate_category_id'
 
-    sequence = fields.Integer(string="Protocolary weight")
+    sequence = fields.Integer(string="Formal position")
 
     def _join_mandate(self):
         res = super()._join_mandate()
--- odoo/addons/mozaik_mandate_category_sequence/models/mandate_category.py
+++ odoo/addons/mozaik_mandate_category_sequence/models/mandate_category.py
@@ -1,11 +1,11 @@
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models
 
 
 class MandateCategory(models.Model):
 
     _inherit = 'mandate.category'
 
-    sequence = fields.Integer(default=99999, string="Protocolary weight")
+    sequence = fields.Integer(default=99999, string="Formal position")
