bcecedf671b595e9a57777d50f6232a3d2757657
Quentin Groulard <quentin.groulard@acsone.eu>
2021-08-03 16:54:23 +0200

    [IMP] mozaik_partner_assembly: run pre-commit

--- mozaik_partner_assembly/__manifest__.py
+++ mozaik_partner_assembly/__manifest__.py
@@ -1,19 +1,19 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Mozaik: Partner Assembly',
-    'summary': """
+    "name": "Mozaik: Partner Assembly",
+    "summary": """
         Add a field 'is_assembly' to a partner""",
-    'version': '14.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV',
-    'website': 'https://github.com/OCA/mozaik',
-    'category': 'Political Association',
-    'depends': [
-        'base',
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/OCA/mozaik",
+    "category": "Political Association",
+    "depends": [
+        "base",
     ],
-    'data': [
-        'views/res_partner.xml',
+    "data": [
+        "views/res_partner.xml",
     ],
     "installable": True,
 }
--- mozaik_partner_assembly/tests/test_res_partner.py
+++ mozaik_partner_assembly/tests/test_res_partner.py
@@ -2,32 +2,40 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import exceptions
-
 from odoo.tests.common import TransactionCase
 
 
 class TestResPartner(TransactionCase):
-
     def test_check_is_assembly(self):
         """
         Check consistency between is_assembly and is_company
         """
         # C=True, A=True: Ok
-        tastevin = self.env['res.partner'].create({
-            'name': 'Confrérie des chevaliers du Tastevin',
-            'is_company': True,
-            'is_assembly': True,
-        })
+        tastevin = self.env["res.partner"].create(
+            {
+                "name": "Confrérie des chevaliers du Tastevin",
+                "is_company": True,
+                "is_assembly": True,
+            }
+        )
         # C=True, A=False: Ok
-        tastevin.write({
-            'is_assembly': False,
-        })
+        tastevin.write(
+            {
+                "is_assembly": False,
+            }
+        )
         # C=False, A=False: Ok
-        tastevin.write({
-            'is_company': False,
-        })
+        tastevin.write(
+            {
+                "is_company": False,
+            }
+        )
         # C=False, A=True: Nok
-        self.assertRaises(exceptions.ValidationError, tastevin.write, vals={
-            'is_assembly': True,
-        })
+        self.assertRaises(
+            exceptions.ValidationError,
+            tastevin.write,
+            vals={
+                "is_assembly": True,
+            },
+        )
         return
