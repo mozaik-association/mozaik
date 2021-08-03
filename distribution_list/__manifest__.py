e82f32b773fb5a42156f9cafc9394352cf6d09bf
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-08-03 15:44:36 +0200

    Apply dotfiles

--- distribution_list/__manifest__.py
+++ distribution_list/__manifest__.py
@@ -8,7 +8,7 @@
     'version': '14.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV',
-    'website': 'https://acsone.eu/',
+    'website': 'https://github.com/OCA/mozaik',
     'category': 'Marketing',
     'depends': [
         'base',
