e5e6d5984dfc570deb76fc8e496d136255dd7c51
Quentin Groulard <quentin.groulard@acsone.eu>
2021-08-11 13:58:53 +0200

    [IMP] inherit_abstract_view: run pre-commit

--- inherit_abstract_view/__manifest__.py
+++ inherit_abstract_view/__manifest__.py
@@ -1,16 +1,16 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Inherit Abstract View',
-    'summary': """
+    "name": "Inherit Abstract View",
+    "summary": """
         Allow a view to inherit an abstract view
         (defined for a abstract model)""",
-    'version': "14.0.1.0.0",
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV',
-    'website': 'https://github.com/OCA/mozaik',
-    'depends': [
-        'base',
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/OCA/mozaik",
+    "depends": [
+        "base",
     ],
     "installable": True,
 }
