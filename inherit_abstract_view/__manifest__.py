6d70920c814aeef1f8c569b596669c859ac3eb25
Quentin Groulard <quentin.groulard@acsone.eu>
2021-08-11 13:57:27 +0200

    [MIG] inherit_abstract_view: Migration to 14.0

--- inherit_abstract_view/__manifest__.py
+++ inherit_abstract_view/__manifest__.py
@@ -5,12 +5,12 @@
     'summary': """
         Allow a view to inherit an abstract view
         (defined for a abstract model)""",
-    'version': '14.0.1.0.0',
+    'version': "14.0.1.0.0",
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV',
     'website': 'https://github.com/OCA/mozaik',
     'depends': [
         'base',
     ],
-    "installable": False,
+    "installable": True,
 }
