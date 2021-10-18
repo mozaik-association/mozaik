56b955e7d973255a162c0a0a10bce0786df5c639
mle <marie.lejeune@acsone.eu>
2021-10-18 15:50:12 +0200

    [ADD] Add mozaik_survey

---
+++ mozaik_survey/models/survey_survey.py
@@ -0,0 +1,12 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SurveySurvey(models.Model):
+
+    _inherit = "survey.survey"
+
+    publish_date = fields.Date(string="Publish Date")
+    interest_ids = fields.Many2many("thesaurus.term", string="Interests")
