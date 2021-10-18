ca6b19c5c43398025bb1247f1fc0bb0954f71915
Adrien Rami <adrien.rami@ecolo.be>
2021-10-18 14:33:22 +0200

    [MIG] user_bypass_security: Migration to 14.0

--- user_bypass_security/__manifest__.py
+++ user_bypass_security/__manifest__.py
@@ -5,12 +5,12 @@
     'name': 'User Bypass Security',
     'description': """
         bypass security for user""",
-    'version': '14.0.1.0.0',
+    'version': "14.0.1.0.0",
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV',
     'website': 'https://github.com/OCA/mozaik',
     'depends': [
         'base'
     ],
-    "installable": False,
+    "installable": True,
 }
