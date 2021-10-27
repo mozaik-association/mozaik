a85ee49ec018d31726f113c5a76dd5b6f1c0e9ee
Denis Robinet (ACSONE) <denis.robinet@acsone.eu>
2021-10-27 08:42:36 +0200

    [IMP] mass_mail_queue_job: pre-commit execution

---
+++ email_template_configurator/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Email Template Configurator",
+    "summary": """
+        Simplifies use of placeholders in email templates""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/mozaik",
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        "security/email_template_placeholder.xml",
+        "views/email_template_placeholder.xml",
+        "views/mail_template.xml",
+    ],
+    "installable": True,
+}
