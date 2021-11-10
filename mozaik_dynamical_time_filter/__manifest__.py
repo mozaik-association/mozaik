7db54dac5ad16a663e5f53ca7b1e382ce91b13ff
mle <marie.lejeune@acsone.eu>
2021-11-10 09:45:06 +0100

    [ADD] mozaik_dynamical_time_filter: dynamic time filters for distribution lists
    refs #53580

---
+++ mozaik_dynamical_time_filter/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mozaik Dynamical Time Filter",
+    "summary": """
+        Implements dynamic time filters""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/OCA/mozaik",
+    "depends": [
+        "mozaik_communication",
+    ],
+    "data": [
+        "views/distribution_list_line.xml",
+    ],
+    "demo": [],
+}
