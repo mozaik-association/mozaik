fc6649c88f4718f6b962a4fd2ffc0e7590af8d0d
Denis Robinet (ACSONE) <denis.robinet@acsone.eu>
2021-11-18 09:23:32 +0100

    [MIG] mozaik_virtual_assembly_instance: Migration to 14.0

--- mozaik_virtual_assembly_instance/__manifest__.py
+++ mozaik_virtual_assembly_instance/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Mozaik: Virtual assembly instance',
-    'version': '14.0.1.0.0',
+    'version': "14.0.1.0.0",
     "author": "ACSONE SA/NV",
     "maintainer": "ACSONE SA/NV",
     "website": "https://github.com/OCA/mozaik",
@@ -20,5 +20,5 @@
         'views/virtual_assembly_instance.xml',
     ],
     'license': 'AGPL-3',
-    "installable": False,
+    "installable": True,
 }
