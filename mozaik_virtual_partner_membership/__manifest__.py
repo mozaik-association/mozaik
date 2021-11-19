5f70fddaaaf7942dc94704ec0296c5711984548f
Denis Robinet (ACSONE) <denis.robinet@acsone.eu>
2021-11-19 09:58:22 +0100

    [MIG] mozaik_virtual_partner_membership: Migration to 14.0

--- mozaik_virtual_partner_membership/__manifest__.py
+++ mozaik_virtual_partner_membership/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Mozaik: Virtual partner membership',
-    'version': '14.0.1.0.0',
+    'version': "14.0.1.0.0",
     "author": "ACSONE SA/NV",
     "maintainer": "ACSONE SA/NV",
     "website": "https://github.com/OCA/mozaik",
@@ -19,5 +19,5 @@
         'views/virtual_partner_membership.xml',
     ],
     'license': 'AGPL-3',
-    "installable": False,
+    "installable": True,
 }
