5171937dc22347182afc38f4e40d7a95b8367dfd
mle <marie.lejeune@acsone.eu>
2021-12-08 11:55:52 +0100

    [ADD] mozaik_petition_question_involvement_category

---
+++ mozaik_petition_question_involvement_category/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mozaik Petition Question Involvement Category",
+    "summary": """
+        Adds an involvement category (not mandatory) on questions and answers.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/OCA/mozaik",
+    "depends": [
+        "mozaik_involvement",
+        "mozaik_petition",
+    ],
+    "data": [
+        "views/petition_question.xml",
+        "views/petition_petition.xml",
+    ],
+    "demo": [],
+}
