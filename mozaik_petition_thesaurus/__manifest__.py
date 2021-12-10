4f3143b62ddf4749ad2a9fb39540562ada88816b
mle <marie.lejeune@acsone.eu>
2021-12-10 14:09:48 +0100

    [ADD] mozaik_event_thesaurus, mozaik_petition_thesaurus, mozaik_survey_thesaurus

---
+++ mozaik_petition_thesaurus/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mozaik Petition Thesaurus",
+    "summary": """
+        Adds interests on a petition""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/OCA/mozaik",
+    "depends": [
+        "mozaik_petition",
+        "mozaik_thesaurus",
+    ],
+    "data": [
+        "views/petition_petition.xml",
+    ],
+    "demo": [],
+}
