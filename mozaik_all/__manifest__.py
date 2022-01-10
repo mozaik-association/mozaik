77bbcf86b972380fb074dcbfc88fca5bee0d4d13
mle <marie.lejeune@acsone.eu>
2022-01-10 16:06:15 +0100

    [ADD] mozaik_product_template: tree view is sorted by sequence
    refs #55698

--- mozaik_all/__manifest__.py
+++ mozaik_all/__manifest__.py
@@ -50,6 +50,7 @@
         "mozaik_partner_fields",
         # 'mozaik_partner_unauthorized',
         "mozaik_person",
+        "mozaik_product_template",
         # 'mozaik_relation_coordinate',
         "mozaik_structure",
         # 'mozaik_subscription_price',
