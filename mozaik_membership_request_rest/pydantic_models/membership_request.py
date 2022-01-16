4bf31b9c521406b1e08ba9623993e97fcbebf686
Adrien Rami <adrien.rami@ecolo.be>
2022-01-16 19:26:38 +0100

    [CHG] Add parameter to model; Change method to POST;

--- mozaik_membership_request_rest/pydantic_models/membership_request.py
+++ mozaik_membership_request_rest/pydantic_models/membership_request.py
@@ -2,27 +2,44 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo.addons.pydantic import models, utils
-from datetime import datetime
+from typing import (List)
 
 class MembershipRequest(models.BaseModel):
     lastname: str
     firstname: str
     gender: str
-    local_only: bool
+    street_man: str
+    zip_man: str
+    city_man: str
     request_type: str
-    distribution_list_ids: int
+    number: str
+    box: str
+    local_only: bool
     day: int
     month: int
     year: int
-    nationality_id: int #many2one
-    phone: str
-    mobile: str
     email: str
-    country_id: int #many2one
-    address_local_street_id: int #many2one
-    zip_man: str
-    street_man: str
-    street2: str
-    number: str
-    box: str
+    mobile: str
+    phone: str
+    interest_ids: List[int]
+    competency_ids: List[int]
+    note: str
+    newsletters: bool
+    is_company: bool
+    #involvements: List[int]
+    local_voluntary: bool
+    regional_voluntary: bool
+    national_voluntary: bool
+    amount: float
+    reference: str
     autovalidate: bool
+    effective_time: str
+    local_only: bool
+    nationality_id: int
+    #journal: bool
+    #indexation_comments: str
+
+
+
+
+
