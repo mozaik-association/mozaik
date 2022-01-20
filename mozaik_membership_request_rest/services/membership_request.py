3dd71203a0d29fd06d2ad741944c125a8be0cc37
Adrien Rami <adrien.rami@ecolo.be>
2022-01-20 13:09:47 +0100

    [ADD] Manage distribution_list_ids, nationality, unit test;

--- mozaik_membership_request_rest/services/membership_request.py
+++ mozaik_membership_request_rest/services/membership_request.py
@@ -1,21 +1,23 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from typing import List
 
-from odoo import _
-from odoo.exceptions import ValidationError
+import collections
+import logging
+
+from odoo.exceptions import UserError
 
 from odoo.addons.base_rest import restapi
-from odoo.addons.base_rest_pydantic.restapi import PydanticModel, PydanticModelList
+from odoo.addons.base_rest_pydantic.restapi import PydanticModel
 from odoo.addons.component.core import Component
 
-from ..pydantic_models.membership_request import (
-    MembershipRequest,
-)
-from ..pydantic_models.membership_request_info import (
-    MembershipRequestInfo,
-)
+from ..pydantic_models.membership_request import MembershipRequest
+from ..pydantic_models.membership_request_info import MembershipRequestInfo
+
+_logger = logging.getLogger(__name__)
+trace = _logger.info
+
+
 class MembershipRequestService(Component):
     _inherit = "base.membership.rest.service"
     _name = "membership.request.rest.service"
@@ -24,14 +26,13 @@ class MembershipRequestService(Component):
     _description = __doc__
 
     @restapi.method(
-            routes=[(["/<int:_id>"], "GET")],
-            output_param=PydanticModel(MembershipRequestInfo),
-            auth="public",
+        routes=[(["/<int:_id>"], "GET")],
+        output_param=PydanticModel(MembershipRequestInfo),
+        auth="public",
     )
     def get(self, _id: int) -> MembershipRequestInfo:
-      membership_request = self._get(_id)
-      return MemblocalhershipRequestInfo.from_orm(membership_request)
-
+        membership_request = self._get(_id)
+        return MembershipRequestInfo.from_orm(membership_request)
 
     @restapi.method(
         routes=[(["/membership_request/<int:autovalidate>"], "POST")],
@@ -40,14 +41,57 @@ class MembershipRequestService(Component):
         auth="public",
     )
     def membership_request(
-        self,
-        autovalidate,
-        membership_request: MembershipRequest
+        self, autovalidate, membership_request: MembershipRequest
     ) -> MembershipRequestInfo:
         mr_obj = self.env["membership.request"]
         vals = membership_request.dict()
-        mr = mr_obj.with_context(autoval=True).create(vals)
-        if(autovalidate):
-            mr.validate_request()
+
+        if vals["distribution_list_ids"]:
+            dlists = self.env["distribution.list"].search(
+                [
+                    ("code", "in", vals["distribution_list_ids"]),
+                    ("newsletter", "=", True),
+                ]
+            )
+            if dlists:
+                vals["distribution_list_ids"] = [(6, 0, dlists.ids)]
+            else:
+                del vals["distribution_list_ids"]
+                _logger.info(
+                    "Unknown distribution_list_ids with code %s",
+                    vals["distribution_list_ids"],
+                )
+        if vals["involvement_category_ids"]:
+            if not isinstance(vals["involvement_category_ids"], collections.Iterable):
+                cats = self.env["partner.involvement.category"].search(
+                    [("code", "in", vals["involvement_category_ids"])]
+                )
+            if cats:
+                vals["involvement_category_ids"] = [(6, 0, cats.ids)]
+            else:
+                del vals["involvement_category_ids"]
+                _logger.info(
+                    "Unknown involvements with code %s",
+                    vals["involvement_category_ids"],
+                )
+        if vals["nationality_id"]:
+            nat = self.env["res.country"].search(
+                [("code", "=", vals["nationality_id"])]
+            )
+            if nat:
+                vals["nationality_id"] = nat.id
+            else:
+                del vals["nationality_id"]
+                _logger.info("Unknown nationality with code %s", vals["nationality"])
+        try:
+            mr = mr_obj.with_context(autoval=True).create(vals)
+            if autovalidate:
+                mr.validate_request()
+        except Exception as e:
+            raise UserError(
+                self.env.user.id, "Membership Request", "CREATE ERROR", e.message
+            ) from e
+
+        trace("======= ws answer: %s - %s", "membership_request", mr.id)
 
         return MembershipRequestInfo.from_orm(mr)
