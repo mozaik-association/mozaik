013e7122e89cfb7d18aa959e799b9c24a1649fd7
Adrien Rami <adrien.rami@ecolo.be>
2022-01-28 15:02:57 +0100

    [CHG] Correcting validation check before creation;

--- mozaik_petition_rest/services/petition_registration.py
+++ mozaik_petition_rest/services/petition_registration.py
@@ -28,7 +28,8 @@ class PetitionRegistrationService(Component):
     def register_answer(
         self, petition_registration: PetitionRegistration
     ) -> PetitionRegistrationInfo:
-        # Variable for control
+        # Control variable to check if we can continue treatement of the
+        # request
         answer_validated = True
         # Checking every answer before register it
         for answer in petition_registration.list_answer:
@@ -36,22 +37,20 @@ class PetitionRegistrationService(Component):
                 [("id", "=", answer.question_id)]
             )
             if question:
-                if question.question_type == "simple_choice":
-                    if question.is_mandatory:
+                if question.is_mandatory:
+                    if question.question_type == "simple_choice":
                         if answer.value_answer_id is None:
                             answer_validated = False
                             raise exceptions.ValidationError(
                                 _("Question with id: '%s' , is Mandatory" % question.id)
                             )
-                elif question.question_type == "text_box":
-                    if question.is_mandatory:
+                    elif question.question_type == "text_box":
                         if answer.value_text_box is None:
                             answer_validated = False
                             raise exceptions.ValidationError(
                                 _("Question with id: '%s' , is Mandatory" % question.id)
                             )
-                elif question.question_type == "tickbox":
-                    if question.is_mandatory:
+                    elif question.question_type == "tickbox":
                         if answer.value_tickbox is None:
                             answer_validated = False
                             raise exceptions.ValidationError(
@@ -61,33 +60,40 @@ class PetitionRegistrationService(Component):
                 answer_validated = False
 
         if answer_validated:
+            # if all answer validated creating the petition registration
             vals = petition_registration.dict()
             del vals["list_answer"]
             res = self.env["petition.registration"].create(vals)
             # Store the answer in a variable to create all answer in once
             vals_answer = {}
             for answer in petition_registration.list_answer:
+                question = self.env["petition.question"].search(
+                    [("id", "=", answer.question_id)]
+                )
                 vals_answer[question.id] = {}
                 vals_answer[question.id]["question_id"] = answer.question_id
+
                 vals_answer[question.id]["petition_id"] = [
                     petition_registration.petition_id
                 ]
-                vals_answer[question.id]["registration_id"] = res.petition_id.id
+                # store registration reference in answer
+                vals_answer[question.id]["registration_id"] = res.id
+                # Store value of answer
                 if question.question_type == "simple_choice":
                     vals_answer[question.id]["value_answer_id"] = answer.value_answer_id
                 elif question.question_type == "text_box":
                     vals_answer[question.id]["value_text_box"] = answer.value_text_box
                 elif question.question_type == "tickbox":
                     vals_answer[question.id]["value_tickbox"] = answer.value_tickbox
-
-            answer_list_id = {}
+            # Create a list of reference to the question registered
+            answer_list_id = []
             for answer in vals_answer:
                 registered_answer = self.env["petition.registration.answer"].create(
                     vals_answer[answer]
                 )
-                answer_list_id.append(registered_answer.id)
-
-            vals_answer = {"list_answer": answer_list_id}
+                answer_list_id.append(registered_answer)
+            # Updating registration with answers
+            vals_answer = {"value_answer_id": answer_list_id}
             res.update(vals)
 
             return PetitionRegistrationInfo.from_orm(res)
