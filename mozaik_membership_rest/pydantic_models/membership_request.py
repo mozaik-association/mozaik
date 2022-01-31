a3a30a07fbca9be970bb18a766462529b93ef888
Quentin Groulard <quentin.groulard@acsone.eu>
2022-01-31 22:26:35 +0100

    [ADD] mozaik_membership_rest (WIP)

---
+++ mozaik_membership_rest/pydantic_models/membership_request.py
@@ -0,0 +1,44 @@
+# Copyright 2022 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from typing import List
+
+from odoo.addons.pydantic import models
+
+
+class MembershipRequest(models.BaseModel):
+    lastname: str
+    firstname: str
+    gender: str
+    street_man: str
+    zip_man: str
+    city_man: str
+    request_type: str
+    number: str = None
+    box: str = None
+    local_only: bool
+    day: int = None
+    month: int = None
+    year: int = None
+    email: str = None
+    mobile: str = None
+    phone: str = None
+    interest_ids: List[int] = []
+    competency_ids: List[int] = []
+    note: str = None
+    distribution_list_ids: List[int] = []
+    distribution_list_ids_opt_out: List[int] = []
+    force_global_opt_out: bool = False
+    force_global_opt_in: bool = False
+    is_company: bool = False
+    involvement_category_ids: List[int] = []
+    local_voluntary: bool = False
+    regional_voluntary: bool = False
+    national_voluntary: bool = False
+    amount: float = None
+    reference: str = None
+    effective_time: str = None
+    local_only: bool = None
+    nationality_id: int = None
+    unemployed_change: str = None
+    auto_validate: bool = False
