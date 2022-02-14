33cd767b153dbb47e3c42dc02ef1863affa721f0
mle <marie.lejeune@acsone.eu>
2022-02-14 15:03:12 +0100

    [FIX] uninstall of mozaik_event_is_private, mozaik_survey_is_private

---
+++ mozaik_security_ama/hooks.py
@@ -0,0 +1,35 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
+
+from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
+
+
+def _set_int_instance_ids(cr):
+    _logger.info("Set int_instance_ids on AMA objets")
+    if openupgrade.column_exists(cr, "event_event", "int_instance_id_old"):
+        openupgrade.m2o_to_x2m(
+            cr, "event.event", "event_event", "int_instance_ids", "int_instance_id_old"
+        )
+    if openupgrade.column_exists(cr, "petition_petition", "int_instance_id_old"):
+        openupgrade.m2o_to_x2m(
+            cr,
+            "petition.petition",
+            "petition_petition",
+            "int_instance_ids",
+            "int_instance_id_old",
+        )
+    if openupgrade.column_exists(cr, "survey_survey", "int_instance_id_old"):
+        openupgrade.m2o_to_x2m(
+            cr,
+            "survey.survey",
+            "survey_survey",
+            "int_instance_ids",
+            "int_instance_id_old",
+        )
+
+
+def pre_init_hook(cr):
+    _set_int_instance_ids(cr)
