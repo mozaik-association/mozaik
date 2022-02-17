787a78a7fd7b538766aed75854bbc6f83e7b604d
mle <marie.lejeune@acsone.eu>
2022-02-17 12:05:27 +0100

    [FIX] mozaik_petition_rest: register_answer: only 1 answer per question

--- mozaik_petition_rest/pydantic_models/petition_registration_answer.py
+++ mozaik_petition_rest/pydantic_models/petition_registration_answer.py
@@ -1,7 +1,6 @@
 # Copyright 2021 ACSONE SA/NV
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from typing import List
 
 from extendable_pydantic import ExtendableModelMeta
 from pydantic import BaseModel
@@ -9,6 +8,6 @@ from pydantic import BaseModel
 
 class PetitionRegistrationAnswer(BaseModel, metaclass=ExtendableModelMeta):
     question_id: int
-    value_answer_id: List[int] = None
+    value_answer_id: int = None
     value_text_box: str = None
     value_tickbox: bool = None
