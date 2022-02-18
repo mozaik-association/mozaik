c5ea5ce05097e3de881656b517579dfb23d88424
mle <marie.lejeune@acsone.eu>
2022-02-18 15:43:04 +0100

    [UPD] event registration service: add registered_partner_id

--- mozaik_event_rest/services/event.py
+++ mozaik_event_rest/services/event.py
@@ -1,6 +1,9 @@
 # Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo import _
+from odoo.exceptions import ValidationError
+
 from odoo.addons.component.core import Component
 from odoo.addons.event_rest_api.pydantic_models.event_registration_request import (
     EventRegistrationRequest,
@@ -21,6 +24,24 @@ class EventService(Component):
     def _prepare_event_registration_values(
         self, event, event_registration_request: EventRegistrationRequest
     ) -> dict:
+        """
+        Adding a check: if registered_partner_id is not given,
+        then firstname, lastname and email are mandatory.
+        """
+        if not event_registration_request.registered_partner_id and not all(
+            [
+                event_registration_request.lastname,
+                event_registration_request.firstname,
+                event_registration_request.email,
+            ]
+        ):
+            raise ValidationError(
+                _(
+                    "If registered_partner_id is not given, "
+                    "firstname, lastname and email are mandatory"
+                )
+            )
+
         res = super()._prepare_event_registration_values(
             event, event_registration_request
         )
@@ -39,4 +60,7 @@ class EventService(Component):
                 )
             )
         res["registration_answer_ids"] = answers
+        res["associated_partner_id"] = (
+            event_registration_request.registered_partner_id or False
+        )
         return res
