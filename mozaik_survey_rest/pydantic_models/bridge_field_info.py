e500bfabf407b253307b17a5d6fc130fe8f9839d
mle <marie.lejeune@acsone.eu>
2022-02-21 11:18:31 +0100

    [UPD] mozaik_survey_rest: add force autovalidation

--- mozaik_survey_rest/pydantic_models/bridge_field_info.py
+++ mozaik_survey_rest/pydantic_models/bridge_field_info.py
@@ -1,7 +1,5 @@
 # Copyright 2022 ACSONE SA/NV
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-# Copyright 2022 ACSONE SA/NV
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from extendable_pydantic import ExtendableModelMeta
 from pydantic import BaseModel
