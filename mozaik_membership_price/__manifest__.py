799a1559e522b01c166cdedb6eaeb51d3250b303
mle <marie.lejeune@acsone.eu>
2022-03-02 16:10:36 +0100

    [ADD] mozaik_membership_price

---
+++ mozaik_membership_price/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mozaik Membership Price",
+    "summary": """
+        Add on a partner the price of the active membership.line""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/OCA/mozaik",
+    "depends": [
+        "mozaik_membership",
+        "mozaik_single_instance",
+    ],
+    "data": [],
+    "demo": [],
+}
