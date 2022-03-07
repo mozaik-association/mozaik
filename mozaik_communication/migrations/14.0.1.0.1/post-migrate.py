4630f990f1861a8555ae4b8ffb66a8863b0040c2
mle <marie.lejeune@acsone.eu>
2022-03-07 14:55:03 +0100

    [FIX] mozaik_communication: post-migrate script

--- mozaik_communication/migrations/14.0.1.0.1/post-migrate.py
+++ mozaik_communication/migrations/14.0.1.0.1/post-migrate.py
@@ -10,7 +10,9 @@ _logger = logging.getLogger(__name__)
 @openupgrade.migrate()
 def migrate(env, version):
     cr = env.cr
-    if openupgrade.column_exists(cr, "distribution_list", "int_instance_id_old"):
+    if openupgrade.column_exists(
+        cr, "distribution_list", "int_instance_id_old"
+    ) and not openupgrade.table_exists(cr, "distribution_list_int_instance_rel"):
         openupgrade.m2o_to_x2m(
             cr,
             env["distribution.list"],
