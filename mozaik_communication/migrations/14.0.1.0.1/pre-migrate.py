b82f9176a1d4d383f2670315b4fd9d99bc711220
mle <marie.lejeune@acsone.eu>
2022-03-07 14:35:21 +0100

    [FIX] mozaik_communication: pre-migrate script

--- mozaik_communication/migrations/14.0.1.0.1/pre-migrate.py
+++ mozaik_communication/migrations/14.0.1.0.1/pre-migrate.py
@@ -10,7 +10,9 @@ _logger = logging.getLogger(__name__)
 @openupgrade.migrate()
 def migrate(env, version):
     cr = env.cr
-    if openupgrade.column_exists(cr, "distribution_list", "int_instance_id"):
+    if openupgrade.column_exists(
+        cr, "distribution_list", "int_instance_id"
+    ) and not openupgrade.column_exists(cr, "distribution_list", "int_instance_id_old"):
         openupgrade.rename_columns(
             cr, {"distribution_list": [("int_instance_id", "int_instance_id_old")]}
         )
