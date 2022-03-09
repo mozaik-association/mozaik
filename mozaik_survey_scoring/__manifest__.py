210c3f8805b04a187f41a35b52b283683c645dec
mle <marie.lejeune@acsone.eu>
2022-03-09 15:41:24 +0100

    [ADD] mozaik_survey_scoring: can skip scoring for skipped questions

---
+++ mozaik_survey_scoring/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mozaik Survey Scoring",
+    "summary": """
+        A question with scoring can be excluded from the score
+        if the partner doesn't answer.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/OCA/mozaik",
+    "depends": [
+        "survey",
+    ],
+    "data": [
+        "views/survey_survey.xml",
+    ],
+    "demo": [],
+}
