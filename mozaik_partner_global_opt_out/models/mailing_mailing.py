3072235a4c19541aa10dd9710bd7e882f817c555
mle <marie.lejeune@acsone.eu>
2022-03-22 11:50:05 +0100

    [UPD] mozaik_partner_global_opt_out: add check on include_opt_out_contacts boolean
    refs #58169

--- mozaik_partner_global_opt_out/models/mailing_mailing.py
+++ mozaik_partner_global_opt_out/models/mailing_mailing.py
@@ -1,7 +1,8 @@
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class MailingMailing(models.Model):
@@ -13,6 +14,39 @@ class MailingMailing(models.Model):
         string="Include opt-out contacts",
         help="If True, include contacts whose email is blacklisted.",
     )
+
+    @api.onchange("mailing_model_id")
+    def onchange_include_opt_out_contacts(self):
+        """
+        include_opt_out_contacts cannot be used if mailing_model_id is not
+        distribution.list or res.partner
+        """
+        self.ensure_one()
+        if not self.mailing_model_id or self.mailing_model_id.model not in [
+            "res.partner",
+            "distribution.list",
+        ]:
+            self.include_opt_out_contacts = False
+
+    @api.constrains("mailing_model_id", "include_opt_out_contacts")
+    def _check_distribution_list_ids(self):
+        """
+        include_opt_out_contacts cannot be used if mailing_model_id is not
+        distribution.list or res.partner
+        """
+        for record in self:
+            if (
+                record.include_opt_out_contacts
+                and record.mailing_model_id
+                and record.mailing_model_id.model
+                not in ["res.partner", "distribution.list"]
+            ):
+                raise ValidationError(
+                    _(
+                        "'Include opt-out contacts' cannot be True "
+                        "if mailing model is different from 'Contact' or 'Distribution List'."
+                    )
+                )
 
     def _process_mass_mailing_queue(self):
         """
