19c7a40266130fe2cbe665acc9108822ea909a32
Quentin Groulard <quentin.groulard@acsone.eu>
2022-03-28 14:24:26 +0200

    [ADD] mozaik_web_image_route

--- mozaik_graphql/controllers/__init__.py
+++ mozaik_graphql/controllers/__init__.py
@@ -1,2 +1,1 @@
-from . import binary
 from . import main
---
+++ mozaik_web_image_route/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mozaik Web Image Route",
+    "summary": """
+        New route to access images, meant to be used in APIs.
+        Can be extended to handle autenthication mechanism (e.g. auth_api_key)""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/OCA/mozaik",
+    "depends": ["web"],
+}
