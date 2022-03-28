52d190ce2c46ca857d6ddeb2125424068b1bfa70
Adrien Rami <adrien.rami@ecolo.be>
2022-03-28 08:33:19 +0200

    [ADD] Add involvement categories codes;

---
+++ mozaik_membership_rest/pydantic_models/membership_request.py
@@ -0,0 +1,51 @@
+# Copyright 2022 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from datetime import datetime
+from typing import List
+
+from extendable_pydantic import ExtendableModelMeta
+from pydantic import BaseModel
+
+
+class MembershipRequest(BaseModel, metaclass=ExtendableModelMeta):
+    lastname: str
+    firstname: str
+    request_type: str
+    gender: str = None
+    street: str = None
+    street2: str = None
+    number: str = None
+    box: str = None
+    zip: str = None
+    city: str = None
+    local_only: bool = None
+    day: int = None
+    month: int = None
+    year: int = None
+    email: str = None
+    mobile: str = None
+    phone: str = None
+    interest_ids: List[int] = []
+    competency_ids: List[int] = []
+    note: str = None
+    distribution_list_ids: List[int] = []
+    is_company: bool = None
+    involvement_category_ids: List[int] = []
+    involvement_category_codes: List[str] = []
+    local_voluntary: bool = None
+    regional_voluntary: bool = None
+    national_voluntary: bool = None
+    amount: float = None
+    reference: str = None
+    effective_time: datetime = None
+    local_only: bool = None
+    nationality_id: int = None
+    city_id: int = None
+    country_id: int = None
+    unemployed_change: str = None
+    disabled_change: str = None
+    force_global_opt_out: bool = None
+    force_global_opt_in: bool = None
+    auto_validate: bool = False
+    force_auto_validate: bool = False
---
+++ mozaik_membership_rest/services/service.py
@@ -0,0 +1,26 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _
+from odoo.exceptions import MissingError
+
+from odoo.addons.component.core import AbstractComponent
+
+
+class BaseMembershipRequestService(AbstractComponent):
+    _inherit = "base.rest.service"
+    _name = "base.membership.rest.service"
+    _collection = "membership.rest.services"
+    _expose_model = None
+
+    def _get(self, _id):
+        domain = [("id", "=", _id)]
+        record = (
+            self.env[self._expose_model].with_context(active_test=False).search(domain)
+        )
+        if not record:
+            raise MissingError(
+                _("The record %s %s does not exist") % (self._expose_model, _id)
+            )
+        else:
+            return record
