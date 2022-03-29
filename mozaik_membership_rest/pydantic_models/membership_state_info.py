54ab86e08a47d9a7e0d1be78636013a1331f54af
Adrien Rami <adrien.rami@ecolo.be>
2022-03-29 06:53:22 +0200

    [ADD] Add Involvement categories code on membership rest;

---
+++ mozaik_membership_rest/pydantic_models/membership_state_info.py
@@ -0,0 +1,20 @@
+# Copyright 2022 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from datetime import datetime
+
+from extendable_pydantic import ExtendableModelMeta
+from pydantic import BaseModel
+
+from odoo.addons.pydantic import utils
+
+
+class MembershipStateInfo(BaseModel, metaclass=ExtendableModelMeta):
+    id: int
+    name: str
+    code: str
+    write_date: datetime
+
+    class Config:
+        orm_mode = True
+        getter_dict = utils.GenericOdooGetter
