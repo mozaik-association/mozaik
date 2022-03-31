484ae9384dbe45d40454e6af46385242df5f657f
Quentin Groulard <quentin.groulard@acsone.eu>
2022-03-31 16:50:31 +0200

    [IMP] Petition/Event summary

---
+++ mozaik_event_description/models/event_event.py
@@ -0,0 +1,11 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class EventEvent(models.Model):
+
+    _inherit = "event.event"
+
+    summary = fields.Text(string="Summary")
