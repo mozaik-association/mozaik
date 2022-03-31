8a7a8c3589b1e3bd3a4d8b0dc832178be26117d3
Denis Robinet (ACSONE) <denis.robinet@acsone.eu>
2022-03-31 13:30:47 +0200

    Fix the security for the merge after closing memberships

--- mozaik_membership/wizards/base_partner_merge_automatic_wizard.py
+++ mozaik_membership/wizards/base_partner_merge_automatic_wizard.py
@@ -16,10 +16,21 @@ class BasePartnerMergeAutomaticWizard(models.TransientModel):
             src_partners = partners - dst_partner
         else:
             ordered_partners = self._get_ordered_partner(partners.ids)
+            dst_partner = ordered_partners[-1]
             src_partners = ordered_partners[:-1]
+
+        # since we close the membership we need to keep an instance for the security
+        for p in src_partners:
+            p.force_int_instance_id = p.int_instance_id
+        dst_force_int_instance_id = dst_partner.force_int_instance_id
 
         src_partners.mapped("membership_line_ids")._close(force=True)
 
-        return super(BasePartnerMergeAutomaticWizard, self)._merge(
+        res = super(BasePartnerMergeAutomaticWizard, self)._merge(
             partner_ids, dst_partner, extra_checks
         )
+
+        # do not modify the force_int_instance_id since it should be empty if
+        # there is a membership_line_id
+        dst_partner.force_int_instance_id = dst_force_int_instance_id
+        return res
