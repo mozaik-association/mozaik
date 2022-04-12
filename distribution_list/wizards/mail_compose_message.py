9c59aa172949b65e9caa824e2b1853028287ee11
Quentin Groulard <quentin.groulard@acsone.eu>
2022-04-12 15:17:50 +0200

    [RMV] Mass function send email from distribution list
    This feature has been replaced by mass_mailing.
    mass_mailing_group, used for reporting was based on this feature.
    It has not been adapted to work with mass_mailing since mass_mailing already provides reporting features.

--- distribution_list/wizards/mail_compose_message.py
+++ distribution_list/wizards/mail_compose_message.py
@@ -1,7 +1,6 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
-from odoo.osv import expression
 
 
 class MailComposeMessage(models.TransientModel):
