c868fd5e731ff7a3ac37b53be05647d85ef120cf
Denis Robinet (ACSONE) <denis.robinet@acsone.eu>
2022-04-15 10:09:00 +0200

    Correctly loop on abject creation

--- mozaik_ama_attachment/models/ir_attachment.py
+++ mozaik_ama_attachment/models/ir_attachment.py
@@ -12,8 +12,8 @@ class IrAttachment(models.Model):
         res = super().create(vals)
         # When uploading an image in body_arch in a mass_mailing, res_model equals "model"
         # and not "mailing.mailing".
-        if (
-            res.res_model
+        public = res.filtered(
+            lambda s: s.res_model
             in [
                 "petition.petition",
                 "survey.survey",
@@ -21,7 +21,8 @@ class IrAttachment(models.Model):
                 "mail.template",
                 "model",
             ]
-            and res.index_content == "image"
-        ):
-            res.write({"public": True})
+            and s.index_content == "image"
+        )
+        if public:
+            public.write({"public": True})
         return res
