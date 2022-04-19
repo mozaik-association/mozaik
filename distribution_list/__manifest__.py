9033ef9b99b6b63b4e8f0b52d053820308191e28
Quentin Groulard <quentin.groulard@acsone.eu>
2022-04-19 17:23:08 +0200

    [IMP] Distribution list mig mail_composer wizard

--- distribution_list/__manifest__.py
+++ distribution_list/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": """
         Manage distribution lists composed
         with multiple filters (odoo domain)""",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/OCA/mozaik",
