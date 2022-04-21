cac1394d4eeee2ae11cb4748105e21d703eac609
mle <marie.lejeune@acsone.eu>
2022-04-21 14:28:45 +0200

    [UPD] mozaik_petition_rest: add partner_ids
    refs #57063

--- mozaik_petition_rest/pydantic_models/__init__.py
+++ mozaik_petition_rest/pydantic_models/__init__.py
@@ -1,5 +1,6 @@
 from . import petition_info
 from . import petition_milestone_info
+from . import petition_partner_info
 from . import petition_question_info
 from . import petition_registration_answer
 from . import petition_registration_info
