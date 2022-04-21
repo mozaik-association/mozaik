8d6cb298162257a5285caed86f9cd19f96726a5c
Quentin Groulard <quentin.groulard@acsone.eu>
2022-04-21 12:44:49 +0200

    [FIX] mozaik_partner_fields init

--- mozaik_partner_fields/__init__.py
+++ mozaik_partner_fields/__init__.py
@@ -2,3 +2,4 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
+from .hooks import pre_init_hook, post_init_hook
--- mozaik_partner_fields/hooks.py
+++ mozaik_partner_fields/hooks.py
@@ -1,13 +1,26 @@
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
 
 from openupgradelib import openupgrade
 
+_logger = logging.getLogger(__name__)
 
-@openupgrade.migrate()
-def migrate(env, version):
-    cr = env.cr
+
+def pre_init_hook(cr):
+    _logger.info("Migrate partner birthdate")
+    if openupgrade.column_exists(cr, "res_partner", "birth_date"):
+        cr.execute(
+            """
+          UPDATE res_partner
+          SET birthdate_date=birth_date
+          """
+        )
+
+
+def post_init_hook(cr, registry):
+    _logger.info("Migrate partner marital/civil status")
     if openupgrade.column_exists(cr, "res_partner", "civil_status"):
         openupgrade.map_values(
             cr,
