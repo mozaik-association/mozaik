7b8ec27046f4f748d3db366ee8db0a456bacf9a7
Quentin Groulard <quentin.groulard@acsone.eu>
2022-05-04 12:54:43 +0200

    [IMP] Mig email bounce

---
+++ mozaik_communication/migrations/14.0.1.0.0/post-migrate.py
@@ -0,0 +1,24 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
+
+from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    cr = env.cr
+    if openupgrade.table_exists(cr, "email_coordinate"):
+        cr.execute(
+            """
+            UPDATE res_partner AS p
+            SET email_bounced = e.bounce_counter,
+                email_bounced_date = e.bounce_date,
+                email_bounced_description = e.bounce_description,
+                first_email_bounced_date = e.first_bounce_date
+            FROM email_coordinate AS e
+            WHERE e.partner_id=p.id AND e.email=p.email
+            """
+        )
