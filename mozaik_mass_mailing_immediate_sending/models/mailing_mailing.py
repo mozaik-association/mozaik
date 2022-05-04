c9fd44402a3a3fc53937989d34d98387f1f14040
mle <marie.lejeune@acsone.eu>
2022-05-04 11:41:41 +0200

    [FIX] mozaik_mass_mailing_immediate_sending: timezone issue
    refs #59253

--- mozaik_mass_mailing_immediate_sending/models/mailing_mailing.py
+++ mozaik_mass_mailing_immediate_sending/models/mailing_mailing.py
@@ -1,5 +1,7 @@
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import pytz
+from dateutil.relativedelta import relativedelta
 
 from odoo import fields, models
 
@@ -16,13 +18,18 @@ class MailingMailing(models.Model):
         If next departure is in the past -> don't show the schedule date.
         """
         for record in self:
-            if record.next_departure < fields.Datetime.now():
+            if record.next_departure + relativedelta(minutes=1) < fields.Datetime.now():
                 record.next_departure_msg = (
                     "Other mailings are currently being sent. "
-                    "This mailing will be sent as soon as possible"
+                    "This mailing will be sent as soon as possible."
                 )
             else:
+                next_departure_utc = pytz.utc.localize(record.next_departure)
+                next_departure_now = next_departure_utc.astimezone(
+                    pytz.timezone(self._context.get("tz") or "UTC")
+                )
+                next_departure_now = next_departure_now.strftime("%d/%m/%Y %H:%M:%S")
                 record.next_departure_msg = (
                     "This mailing is scheduled for "
-                    "%(next_departure)s" % {"next_departure": record.next_departure}
+                    "%(next_departure)s" % {"next_departure": next_departure_now}
                 )
