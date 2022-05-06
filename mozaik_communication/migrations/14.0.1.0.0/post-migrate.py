42d5d738584ca993932f346279cb44cce3ac7cd5
Quentin Groulard <quentin.groulard@acsone.eu>
2022-05-06 14:35:17 +0200

    [MIG] Postal bounce

--- mozaik_communication/migrations/14.0.1.0.0/post-migrate.py
+++ mozaik_communication/migrations/14.0.1.0.0/post-migrate.py
@@ -9,6 +9,7 @@ _logger = logging.getLogger(__name__)
 
 @openupgrade.migrate()
 def migrate(env, version):
+    _logger.info("Mig bounce counters")
     cr = env.cr
     if openupgrade.table_exists(cr, "email_coordinate"):
         cr.execute(
@@ -18,5 +19,15 @@ def migrate(env, version):
                 email_bounced_description = e.bounce_description
             FROM email_coordinate AS e
             WHERE e.partner_id=p.id AND e.email=p.email
+            """
+        )
+    if openupgrade.table_exists(cr, "postal_coordinate"):
+        cr.execute(
+            """
+            UPDATE res_partner AS p
+            SET last_postal_failure_date = pc.bounce_date,
+                postal_bounced = true
+            FROM postal_coordinate AS pc
+            WHERE pc.partner_id=p.id AND pc.is_main=true AND pc.bounce_date IS NOT NULL
             """
         )
