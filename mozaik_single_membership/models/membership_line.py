62462e4e0f775ade9f870be3d095af21abec6b12
mle <marie.lejeune@acsone.eu>
2022-08-08 09:12:24 +0200

    [IMP] mozaik_single_membership: code review improvements
    refs #61461

--- mozaik_single_membership/models/membership_line.py
+++ mozaik_single_membership/models/membership_line.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
-from odoo.fields import first
 
 
 class MembershipLine(models.Model):
@@ -13,22 +12,22 @@ class MembershipLine(models.Model):
         comodel_name="membership.state", string="Previous State"
     )
 
-    def _update_previous_state(self):
-        self.ensure_one()
-        partner = self.partner_id
-        previous_line = first(
-            self.env["membership.line"].search(
-                [("partner_id", "=", partner.id), ("date_to", "!=", False)],
-                order="date_to desc, create_date desc",
-            )
+    @api.model
+    def _update_previous_state(self, vals):
+        partner_id = vals.get("partner_id")
+        previous_line = self.env["membership.line"].search(
+            [("partner_id", "=", partner_id), ("date_to", "!=", False)],
+            limit=1,
+            order="date_to desc, create_date desc",
         )
         if previous_line:
-            self.previous_state_id = previous_line.state_id
+            vals["previous_state_id"] = previous_line.state_id.id
         else:
-            self.previous_state_id = self.env["membership.state"]._get_default_state()
+            vals["previous_state_id"] = (
+                self.env["membership.state"]._get_default_state().id
+            )
 
     @api.model
     def create(self, vals):
-        res = super().create(vals)
-        res._update_previous_state()
-        return res
+        self._update_previous_state(vals)
+        return super().create(vals)
