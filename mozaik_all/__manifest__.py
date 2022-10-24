bba4189de728497a4f36262000247f49d8446995
mle <marie.lejeune@acsone.eu>
2022-10-24 10:28:11 +0200

    [IMP] res.partner form views, to improve view inheritance

--- mozaik_all/__manifest__.py
+++ mozaik_all/__manifest__.py
@@ -58,6 +58,7 @@
         "mozaik_partner_disabled",
         "mozaik_partner_fields",
         "mozaik_partner_global_opt_out",
+        "mozaik_partner_personal_information",
         "mozaik_partner_unemployed",
         "mozaik_partner_website",
         # 'mozaik_partner_unauthorized',
