b0a77c37d3bedcf42ce63b9dc91bdd434b1b79d7
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-11-18 17:29:28 +0100

    Fix manifest website keys

--- distribution_list/__manifest__.py
+++ distribution_list/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "14.0.1.1.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
-    "website": "https://github.com/OCA/mozaik",
+    "website": "https://github.com/mozaik-association/mozaik",
     "category": "Marketing",
     "depends": ["base", "web", "mail"],
     "data": [
