4ab4f89b9fb0ab14dbf0fc00e6b4dea6d47b2501
Quentin Groulard <quentin.groulard@acsone.eu>
2022-12-07 13:01:59 +0100

    [ADD] mozaik_account_payment_order_partner_account

---
+++ mozaik_account_payment_order_partner_account/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mozaik Account Payment Order Partner Account",
+    "summary": """
+        This module allows to force an account for
+        partner move lines from debit orders.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/mozaik-association/mozaik",
+    "depends": ["account_payment_order"],
+    "data": ["views/res_config_settings.xml"],
+}
