cbaf3f0d4520622f35b24d44e316949a4224da94
Marie Lejeune <marie.lejeune@acsone.eu>
2025-06-16 12:36:49 +0200

    [IMP] mozaik_event_barcode: computation of event domain was too slow

--- mozaik_event_barcode/models/event_event.py
+++ mozaik_event_barcode/models/event_event.py
@@ -45,29 +45,9 @@ class EventEvent(models.Model):
         """
         self.ensure_one()
         voting_domain = self._get_normalized_domain()
-        virtual_target_ids = (
-            self.env["virtual.partner.membership"].search(voting_domain).ids
-        )
-        if not virtual_target_ids:
-            return []
-        self.env.cr.execute(
-            """
-        SELECT DISTINCT p.id
-        FROM res_partner p
-           JOIN virtual_partner_membership vp
-           ON p.id = vp.partner_id
-        WHERE vp.id in %(vt_list)s;
-        """,
-            {
-                "vt_list": tuple(virtual_target_ids),
-            },
-        )
-        res_ids = [r[0] for r in self.env.cr.fetchall()]
-        if res_ids:
-            return (
-                self.env["res.partner"].search([("id", "in", res_ids)]).ids
-            )  # apply security
-        return []
+        virtual_targets = self.env["virtual.partner.membership"].search(voting_domain)
+        partner_ids = virtual_targets.mapped("partner_id.id")
+        return partner_ids
 
     def reopen_barcode_scanner(self):
         self.ensure_one()
