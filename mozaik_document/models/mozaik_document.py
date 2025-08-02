6d4822cb8258ef4cdbac8b356e1864f55f0aa75d
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2025-08-02 18:35:42 +0200

    [IMP] mozalk_document: improve filesize cmputation performance

--- mozaik_document/models/mozaik_document.py
+++ mozaik_document/models/mozaik_document.py
@@ -23,16 +23,19 @@ class MozaikDocument(models.Model):
 
     @api.depends("content")
     def _compute_content_filesize(self):
-        for doc in self:
-            attachment = (
-                self.env["ir.attachment"]
-                .sudo()
-                .search(
-                    [
-                        ("res_field", "=", "content"),
-                        ("res_model", "=", "mozaik.document"),
-                        ("res_id", "=", doc.id),
-                    ]
-                )
+        attachments = (
+            self.env["ir.attachment"]
+            .sudo()
+            .search(
+                [
+                    ("res_field", "=", "content"),
+                    ("res_model", "=", "mozaik.document"),
+                    ("res_id", "in", self.ids),
+                ]
             )
-            doc.content_filesize = attachment.file_size if attachment else 0
+        )
+        file_size_by_doc_id = {
+            attachment.res_id: attachment.file_size for attachment in attachments
+        }
+        for doc in self:
+            doc.content_filesize = file_size_by_doc_id.get(doc.id, 0)
