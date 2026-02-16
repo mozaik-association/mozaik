86561d2c1e1453875311f333978d035b4f60c34e
Marie Lejeune <marie.lejeune@acsone.eu>
2026-02-16 17:09:43 +0100

    [IMP] light autovalidation: write core of the process

--- mozaik_membership_request_autovalidate_light/models/membership_request.py
+++ mozaik_membership_request_autovalidate_light/models/membership_request.py
@@ -20,6 +20,24 @@ class MembershipRequest(models.Model):
             ("cancel",),
         ]
     )
+    light_mr_id = fields.Many2one(
+        "membership.request",
+        help="Associated light auto-validated membership request linked to this MR.",
+    )
+    complete_mr_id = fields.Many2one(
+        "membership.request",
+        help="Membership request that lead to the generation of this light auto-validated MR.",
+    )
+
+    def action_invalidate(self, vals=None):
+        """
+        Don't invalidate other MR in cascade, otherwise Max Depth Recursion Error
+        between light MR and complete MR.
+        """
+        return super(
+            MembershipRequest,
+            self.with_context(invalidate_keep_active_models="membership.request"),
+        ).action_invalidate(vals)
 
     def write(self, vals):
         """
@@ -31,17 +49,151 @@ class MembershipRequest(models.Model):
         return res
 
     def _check_light_autoval(self, auto_val):
-        self.ensure_one()
-        failure_reason = ""
-        if not auto_val:
-            failure_reason = _("Auto validation manually set to false")
-        # TODO: add checks for deciding if auto-val should be accepted or not
-        return auto_val, failure_reason
+        """
+        Returns a dict containing the following parameters
+        * auto_val is True if auto_validation is permitted, False otherwise
+        * failure reason is the reason why auto_validation is not permitted (empty if auto_val
+        is True)
+        * partner is the matched partner, if found. Else False
 
-    def _light_validate_request(self):
+        See ticket #88373 for light auto-validation details.
+        """
         self.ensure_one()
-        # TODO: copy some fields on a new MR, validate it,
-        #  set the first MR into light_autoval state
+        res = {
+            "auto_val": auto_val,
+            "failure_reason": "",
+            "partner": False,
+        }
+        if not res["auto_val"]:
+            res["failure_reason"] = _("Auto validation manually set to false")
+            return res
+        if not self.email:
+            res.update(
+                {
+                    "auto_val": False,
+                    "failure_reason": _("Email is required for light autovalidation."),
+                }
+            )
+            return res
+        matched_partners = self.env["res.partner"].search(
+            [("email", "=", self.email)], limit=2
+        )
+        if len(matched_partners) > 1:
+            res.update(
+                {
+                    "auto_val": False,
+                    "failure_reason": _(
+                        "Several partners found with email '%s'. Light autovalidation failed."
+                        % self.email
+                    ),
+                }
+            )
+            return res
+        if len(matched_partners) == 1:
+            res["partner"] = matched_partners
+        return res
+
+    def _has_full_address(self):
+        """
+        Return True if one of the fields street/number/... is filled
+        """
+        self.ensure_one()
+        return (
+            self.address_local_street_id
+            or self.street_man
+            or self.street2
+            or self.number
+            or self.box
+            or self.sequence
+        )
+
+    def _prepare_address_vals_for_light_mr(self, matched_partner):
+        """
+        Rules for copying address from the MR to the light MR are the following:
+        - Matched partner & only city and/or zip on MR & no address on matched partner
+          -> copy city and/or zip
+        - No matched partner: copy full address from MR
+        - Other cases: don't copy any address field.
+
+        """
+        self.ensure_one()
+        if (
+            matched_partner
+            and not matched_partner.address_address_id
+            and not self._has_full_address()
+        ):
+            return {
+                "country_id": self.country_id.id,
+                "zip_man": self.zip_man,
+                "city_man": self.city_man,
+                "city_id": self.city_id.id,
+            }
+        if not matched_partner:
+            return {
+                "country_id": self.country_id.id,
+                "zip_man": self.zip_man,
+                "city_man": self.city_man,
+                "city_id": self.city_id.id,
+                "address_local_street_id": self.address_local_street_id.id,
+                "street_man": self.street_man,
+                "street2": self.street2,
+                "number": self.number,
+                "box": self.box,
+                "sequence": self.sequence,
+            }
+        return {}
+
+    def _prepare_vals_for_light_mr(self, matched_partner):
+        """
+        Copy part of the values of self to create the light MR.
+        """
+        # TODO. Check if commands must be better copied
+        #  + Check if all dependencies are included.
+        self.ensure_one()
+        vals = {}
+        if matched_partner:
+            vals.update(
+                {
+                    "lastname": matched_partner.lastname,
+                    "firstname": matched_partner.firstname,
+                    "partner_id": matched_partner.id,
+                }
+            )
+        else:
+            vals.update(
+                {
+                    "lastname": self.lastname,
+                    "firstname": self.firstname,
+                }
+            )
+        vals.update(
+            {
+                # Involvements
+                "effective_time": self.effective_time,
+                "involvement_category_ids": self.involvement_category_ids,
+                # Thesaurus
+                "competency_ids": self.competency_ids,
+                "interest_ids": self.interest_ids,
+                "indexation_comments": self.indexation_comments,
+                # Payment
+                "amount": self.amount,  # TODO: other payment fields
+            }
+        )
+        vals.update(self._prepare_address_vals_for_light_mr(matched_partner))
+
+        return vals
+
+    def _light_validate_request(self, matched_partner):
+        self.ensure_one()
+        # Create light MR
+        create_vals = self._prepare_vals_for_light_mr(matched_partner)
+        light_mr = self.env["membership.request"].create(create_vals)
+        # Link both MR
+        light_mr.complete_mr_id = self
+        self.light_mr_id = light_mr
+        # Validate light MR
+        # TODO: what should we do if the validation of light MR fails?
+        light_mr.validate_request()
         self.write({"state": "light_autoval"})
 
     def _auto_validate(self, auto_val=True):
@@ -56,9 +208,9 @@ class MembershipRequest(models.Model):
         if self.autovalidation_type == "complete":
             return super()._auto_validate(auto_val)
         # Light auto-validation
-        auto_val, failure_reason = self._check_light_autoval(auto_val)
-        if auto_val:
+        light_autoval_res = self._check_light_autoval(auto_val)
+        if light_autoval_res["auto_val"]:
             # automatic light validation
-            self._light_validate_request()
+            self._light_validate_request(matched_partner=light_autoval_res["partner"])
 
-        return failure_reason
+        return light_autoval_res["failure_reason"]
---
+++ mozaik_membership_request_autovalidate_light/tests/test_membership_request.py
@@ -0,0 +1,465 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from datetime import datetime
+
+from odoo.tests.common import SavepointCase
+
+
+class TestMembershipRequest(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.mr_model = cls.env["membership.request"].with_context(mode="pre_process")
+        cls.belgium = cls.env["res.country"].search([("code", "=", "BE")])
+        cls.city_lg = cls.env["res.city"].create(
+            {
+                "name": "Liège",
+                "zipcode": "4000",
+                "country_id": cls.belgium.id,
+            }
+        )
+        cls.city_namur = cls.env["res.city"].create(
+            {
+                "name": "Namur",
+                "zipcode": "5000",
+                "country_id": cls.belgium.id,
+            }
+        )
+        cls.local_street = cls.env["address.local.street"].create(
+            {
+                "local_zip": "4000",
+                "local_street": "Rue du Puits",
+                "identifier": "puits",
+            }
+        )
+        cls.local_street_namur = cls.env["address.local.street"].create(
+            {
+                "local_zip": "5000",
+                "local_street": "Rue du Moulin",
+                "identifier": "moulin",
+            }
+        )
+        cls.federal = cls.env.ref("mozaik_structure.int_instance_01")
+        cls.omar_sy = cls.env["res.partner"].create(
+            {
+                "lastname": "Sy",
+                "firstname": "Omar",
+                "email": "omarsy@test.com",
+            }
+        )
+
+    def test_autoval_failed_no_autoval_type(self):
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": False,
+                "lastname": "DUJARDIN",
+                "firstname": "Jean",
+                "gender": "male",
+                "street_man": False,
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+                "email": "jean@duj.fr",
+            }
+        )
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(mr.partner_id)
+        self.assertEqual("confirm", mr.state)
+        self.assertEqual(
+            failure_reason,
+            "No autovalidation (neither complete or partial) selected on the MR.",
+        )
+
+    def test_light_autoval_failed_explicit_skip(self):
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "DUJARDIN",
+                "firstname": "Jean",
+                "gender": "male",
+                "street_man": False,
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+                "email": "jean@duj.fr",
+            }
+        )
+        failure_reason = mr._auto_validate(False)
+        self.assertFalse(mr.partner_id)
+        self.assertEqual("confirm", mr.state)
+        self.assertEqual(failure_reason, "Auto validation manually set to false")
+
+    def test_light_autoval_failed_no_email(self):
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "DUJARDIN",
+                "firstname": "Jean",
+                "gender": "male",
+                "street_man": False,
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+            }
+        )
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(mr.partner_id)
+        self.assertEqual("confirm", mr.state)
+        self.assertEqual(failure_reason, "Email is required for light autovalidation.")
+
+    def test_light_autoval_failed_several_matched_partners(self):
+        self.env["res.partner"].create(
+            [
+                {
+                    "lastname": "Dumoulin",
+                    "firstname": "Jean",
+                    "email": "jd@test.com",
+                },
+                {
+                    "lastname": "Dupont",
+                    "firstname": "Jean",
+                    "email": "jd@test.com",
+                },
+            ]
+        )
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "DUJARDIN",
+                "firstname": "Jean",
+                "email": "jd@test.com",
+                "gender": "male",
+                "street_man": False,
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+            }
+        )
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(mr.partner_id)
+        self.assertEqual("confirm", mr.state)
+        self.assertEqual(
+            failure_reason,
+            "Several partners found with email 'jd@test.com'. Light autovalidation failed.",
+        )
+
+    def test_light_autoval_involvements(self):
+        """
+        Check light auto-validation validates the involvements
+        """
+        ic_1 = self.env["partner.involvement.category"].create(
+            {
+                "name": "First IC",
+            }
+        )
+        ic_2 = self.env["partner.involvement.category"].create(
+            {
+                "name": "Second IC",
+            }
+        )
+        inv_effective_time = datetime(2026, 2, 16, 13)
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "Sy",
+                "firstname": "Omar",
+                "email": "omarsy@test.com",
+                "gender": "male",
+                "street_man": False,
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+                "involvement_category_ids": [(4, ic_1.id), (4, ic_2.id)],
+                "effective_time": inv_effective_time,
+            }
+        )
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        self.assertEqual(mr.state, "light_autoval")
+        self.assertFalse(mr.active)
+        self.assertEqual(mr.light_mr_id.state, "validate")
+        self.assertFalse(mr.light_mr_id.active)
+        self.assertEqual(mr.light_mr_id.partner_id, self.omar_sy)
+        self.assertEqual(len(self.omar_sy.partner_involvement_ids), 2)
+        self.assertEqual(
+            set(
+                self.omar_sy.partner_involvement_ids.mapped(
+                    "involvement_category_id.id"
+                )
+            ),
+            {ic_1.id, ic_2.id},
+        )
+        self.assertEqual(
+            self.omar_sy.partner_involvement_ids[0].effective_time, inv_effective_time
+        )
+        self.assertEqual(
+            self.omar_sy.partner_involvement_ids[1].effective_time, inv_effective_time
+        )
+
+    def test_light_autoval_indexation_comments(self):
+        competency_1 = self.env["thesaurus.term"].create({"name": "Competency 1"})
+        competency_2 = self.env["thesaurus.term"].create({"name": "Competency 2"})
+        interest_1 = self.env["thesaurus.term"].create({"name": "Interest 1"})
+        interest_2 = self.env["thesaurus.term"].create({"name": "Interest 2"})
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "Omar",
+                "firstname": "Sy",
+                "partner_id": self.omar_sy.id,
+                "email": "omarsy@test.com",
+                "gender": "male",
+                "request_type": "m",
+                "indexation_comments": "Comment",
+            }
+        )
+        mr.write(
+            {
+                "competency_ids": [(4, competency_1.id), (4, competency_2.id)],
+                "interest_ids": [(4, interest_1.id), (4, interest_2.id)],
+            }
+        )
+        # TODO: clarify if the process is normal: with pre-process indexation
+        #  & comments are cleaned
+        # if the partner doesn't have existing competency or interest.
+        # Should we always light auto-validate with no pre-process?
+        failure_reason = mr.with_context(mode="no_pre_process")._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        self.assertEqual(mr.state, "light_autoval")
+        self.assertFalse(mr.active)
+        self.assertEqual(mr.light_mr_id.state, "validate")
+        self.assertFalse(mr.light_mr_id.active)
+        self.assertEqual(mr.light_mr_id.partner_id, self.omar_sy)
+        self.assertEqual(len(self.omar_sy.competency_ids), 2)
+        self.assertEqual(
+            set(self.omar_sy.competency_ids.ids), {competency_1.id, competency_2.id}
+        )
+        self.assertEqual(len(self.omar_sy.interest_ids), 2)
+        self.assertEqual(
+            set(self.omar_sy.interest_ids.ids), {interest_1.id, interest_2.id}
+        )
+        self.assertEqual(self.omar_sy.indexation_comments, "Comment")
+
+    # def test_light_autoval_payment(self):
+    #     # TODO depending on Ecolo's answer
+    #     self.assertTrue(False)
+
+    def test_light_autoval_new_partner_partial_address(self):
+        """
+        Check that new partner is created with partial address
+        """
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "DUJARDIN",
+                "firstname": "Jean",
+                "email": "jd@test.com",
+                "gender": "male",
+                "street_man": False,
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+            }
+        )
+        last_id = self.env["res.partner"].search([], order="id desc", limit=1).id
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        partner = mr.light_mr_id.partner_id
+        self.assertGreater(partner.id, last_id)
+        self.assertTrue(partner.address_address_id)
+        self.assertEqual(partner.address_address_id.city_id, self.city_lg)
+
+    def test_light_autoval_new_partner_full_address_local_street(self):
+        """
+        Check that new partner is created with full address
+        (street from address.local.street)
+        """
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "DUJARDIN",
+                "firstname": "Jean",
+                "email": "jd@test.com",
+                "gender": "male",
+                "address_local_street_id": self.local_street.id,
+                "number": "12",
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+            }
+        )
+        last_id = self.env["res.partner"].search([], order="id desc", limit=1).id
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        partner = mr.light_mr_id.partner_id
+        self.assertGreater(partner.id, last_id)
+        self.assertTrue(partner.address_address_id)
+        self.assertEqual(partner.address_address_id.city_id, self.city_lg)
+        self.assertEqual(
+            partner.address_address_id.address_local_street_id, self.local_street
+        )
+
+    def test_light_autoval_new_partner_full_address_manual_street(self):
+        """
+        Check that new partner is created with full address (manual street)
+        """
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "DUJARDIN",
+                "firstname": "Jean",
+                "email": "jd@test.com",
+                "gender": "male",
+                "street_man": "Rue du Puits",
+                "number": "12",
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+            }
+        )
+        last_id = self.env["res.partner"].search([], order="id desc", limit=1).id
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        self.assertEqual(mr.light_mr_id.state, "validate")
+        partner = mr.light_mr_id.partner_id
+        self.assertGreater(partner.id, last_id)
+        self.assertTrue(partner.address_address_id)
+        self.assertEqual(partner.address_address_id.city_id, self.city_lg)
+        self.assertEqual(partner.address_address_id.street_man, "Rue du Puits")
+
+    def test_light_autoval_matched_partner_no_address_mr_partial_address(self):
+        """
+        Matched partner has no address and MR has partial address
+        -> Set partial address on partner
+        """
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "Sy",
+                "firstname": "Omar",
+                "email": "omarsy@test.com",
+                "gender": "male",
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+            }
+        )
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        self.assertEqual(mr.light_mr_id.state, "validate")
+        self.assertTrue(self.omar_sy.address_address_id)
+        self.assertEqual(self.omar_sy.address_address_id.city_id, self.city_lg)
+
+    def test_light_autoval_matched_partner_no_address_mr_full_address(self):
+        """
+        Matched partner has no address and MR has full address -> No change
+        """
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "Sy",
+                "firstname": "Omar",
+                "email": "omarsy@test.com",
+                "gender": "male",
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "street_man": "Rue du Puits",
+                "number": "12",
+                "request_type": "m",
+            }
+        )
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        self.assertEqual(mr.light_mr_id.state, "validate")
+        self.assertFalse(self.omar_sy.address_address_id)
+
+    def test_light_autoval_matched_partner_has_address_mr_partial_address(self):
+        """
+        Matched partner has an address and MR has partial address -> No change
+        """
+        self.omar_sy.address_address_id = self.env["address.address"].create(
+            {
+                "country_id": self.belgium.id,
+                "city_id": self.city_namur.id,
+                "address_local_street_id": self.local_street_namur.id,
+                "number": "15",
+            }
+        )
+        self.assertEqual(self.omar_sy.address_address_id.city_id, self.city_namur)
+        # Partial address on MR
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "Sy",
+                "firstname": "Omar",
+                "email": "omarsy@test.com",
+                "gender": "male",
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "request_type": "m",
+            }
+        )
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        self.assertEqual(mr.light_mr_id.state, "validate")
+        self.assertEqual(self.omar_sy.address_address_id.city_id, self.city_namur)
+        # Full address on MR
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "Sy",
+                "firstname": "Omar",
+                "email": "omarsy@test.com",
+                "gender": "male",
+                "zip_man": "4000",
+                "city_id": self.city_lg.id,
+                "address_local_street_id": self.local_street.id,
+                "number": "8",
+                "request_type": "m",
+            }
+        )
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        self.assertEqual(mr.light_mr_id.state, "validate")
+        self.assertEqual(self.omar_sy.address_address_id.city_id, self.city_namur)
+        self.assertEqual(
+            self.omar_sy.address_address_id.address_local_street_id,
+            self.local_street_namur,
+        )
+
+    def test_matched_partner_name_differs(self):
+        """
+        If matched partner but names differs, don't change name.
+        """
+        mr = self.mr_model.create(
+            {
+                "autovalidation_type": "light",
+                "lastname": "Othername",
+                "firstname": "Otherfirstname",
+                "email": "omarsy@test.com",
+                "gender": "male",
+                "request_type": "m",
+            }
+        )
+        failure_reason = mr._auto_validate(True)
+        self.assertFalse(failure_reason)
+        self.assertTrue(mr.light_mr_id)
+        self.assertEqual(mr.state, "light_autoval")
+        self.assertFalse(mr.active)
+        self.assertEqual(mr.light_mr_id.state, "validate")
+        self.assertFalse(mr.light_mr_id.active)
+        self.assertEqual(mr.light_mr_id.partner_id, self.omar_sy)
+        self.assertEqual(mr.light_mr_id.lastname, "Sy")
+        self.assertEqual(mr.light_mr_id.firstname, "Omar")
+        self.assertEqual(self.omar_sy.lastname, "Sy")
+        self.assertEqual(self.omar_sy.firstname, "Omar")
