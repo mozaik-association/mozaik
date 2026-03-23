6def1f7f5a02f6d5b56e497e42a569d725d5f5cc
Quentin Groulard <quentin.groulard@acsone.eu>
2026-03-23 15:08:02 +0100

    [ADD] mozaik_event_registration_counter

---
+++ mozaik_event_registration_counter/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mozaik Event Registration Counter",
+    "summary": """
+        Handle event registration directly made at the counter.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/mozaik-association/mozaik",
+    "depends": [
+        "event",
+    ],
+    "data": ["views/event_registration.xml"],
+    "demo": [],
+}
