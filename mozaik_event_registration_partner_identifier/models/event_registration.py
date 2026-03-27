daf28ec7cfe80fb81567eb08093f9f7ebe29d015
Quentin Groulard <quentin.groulard@acsone.eu>
2026-03-27 14:35:43 +0100

    [IMP] mozaik_event_registration_partner_identifier: Make partner_identifier compute
    To ease its overwriting

--- mozaik_event_registration_partner_identifier/models/event_registration.py
+++ mozaik_event_registration_partner_identifier/models/event_registration.py
@@ -1,12 +1,15 @@
 # Copyright 2026 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EventRegistration(models.Model):
     _inherit = "event.registration"
 
-    partner_identifier = fields.Char(
-        string="Partner Identifier", related="partner_id.identifier", store=True
-    )
+    partner_identifier = fields.Char(compute="_compute_partner_identifier", store=True)
+
+    @api.depends("partner_id")
+    def _compute_partner_identifier(self):
+        for rec in self:
+            rec.partner_identifier = rec.partner_id.identifier
